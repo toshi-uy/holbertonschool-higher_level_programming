83c3e3c5cc2026bc23ce7bae985c9fba77b7ff46
toshi-uy <s_borgia@hotmail.com>
2021-04-27 22:40:41 +0000

    erased for

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
-    for i in str:
-        if n >= 0:
-            new = str[:n] + str[n + 1:]
-            return new
-        else:
-            return str
+    if n >= 0:
+        new = str[:n] + str[n + 1:]
+        return new
+    else:
+        return str
