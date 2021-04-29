e66b3703ecc84da0fcf50ba9bca2505c8eee8a26
toshi-uy <s_borgia@hotmail.com>
2021-04-29 17:49:49 +0000

    changed * condition and added int casting

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    from calculator_1 import add, sub, mul, div
+    n = len(sys.argv)
+    if n != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+    if sys.argv[2] == "+":
+        print("{} {} {} = {}".format(int(sys.argv[1]), sys.argv[2], \
+int(sys.argv[3]), add(int(sys.argv[1]), int(sys.argv[3]))))
+    elif sys.argv[2] == "-":
+        print("{} {} {} = {}".format(int(sys.argv[1]), sys.argv[2], \
+int(sys.argv[3]), sub(int(sys.argv[1]), int(sys.argv[3]))))
+    elif sys.argv[2] == "*":
+        print("{} {} {} = {}".format(int(sys.argv[1]), sys.argv[2], \
+int(sys.argv[3]), mul(int(sys.argv[1]), int(sys.argv[3]))))
+    elif sys.argv[2] == "/":
+        print("{} {} {} = {}".format(int(sys.argv[1]), sys.argv[2], \
+int(sys.argv[3]), div(int(sys.argv[1]), int(sys.argv[3]))))
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
