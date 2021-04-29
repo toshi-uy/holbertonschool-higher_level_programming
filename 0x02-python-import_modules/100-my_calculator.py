62a3d46ab2c30ef5c2293ac8853a795d7f4fa1e5
toshi-uy <s_borgia@hotmail.com>
2021-04-29 17:52:48 +0000

    fixed pepocho

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -7,17 +7,21 @@ if __name__ == "__main__":
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
     if sys.argv[2] == "+":
-        print("{} {} {} = {}".format(int(sys.argv[1]), sys.argv[2], \
-int(sys.argv[3]), add(int(sys.argv[1]), int(sys.argv[3]))))
+        print("{} {} {} = {}".format(int(sys.argv[1]), sys.argv[2],
+                                     int(sys.argv[3]), add(int(sys.argv[1]),
+                                                           int(sys.argv[3]))))
     elif sys.argv[2] == "-":
-        print("{} {} {} = {}".format(int(sys.argv[1]), sys.argv[2], \
-int(sys.argv[3]), sub(int(sys.argv[1]), int(sys.argv[3]))))
+        print("{} {} {} = {}".format(int(sys.argv[1]), sys.argv[2],
+                                     int(sys.argv[3]), sub(int(sys.argv[1]),
+                                                           int(sys.argv[3]))))
     elif sys.argv[2] == "*":
-        print("{} {} {} = {}".format(int(sys.argv[1]), sys.argv[2], \
-int(sys.argv[3]), mul(int(sys.argv[1]), int(sys.argv[3]))))
+        print("{} {} {} = {}".format(int(sys.argv[1]), sys.argv[2],
+                                     int(sys.argv[3]), mul(int(sys.argv[1]),
+                                                           int(sys.argv[3]))))
     elif sys.argv[2] == "/":
-        print("{} {} {} = {}".format(int(sys.argv[1]), sys.argv[2], \
-int(sys.argv[3]), div(int(sys.argv[1]), int(sys.argv[3]))))
+        print("{} {} {} = {}".format(int(sys.argv[1]), sys.argv[2],
+                                     int(sys.argv[3]), div(int(sys.argv[1]),
+                                                           int(sys.argv[3]))))
     else:
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
