c919833832aa6b3f288b701d28b6259253ca2a07
toshi-uy <s_borgia@hotmail.com>
2021-05-04 18:51:57 +0000

    changed and for or in condition

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if idx > 0 and idx <= len(my_list) - 1:
+    if idx > 0 or idx <= len(my_list) - 1:
         return my_list[idx]
     else:
         return None
