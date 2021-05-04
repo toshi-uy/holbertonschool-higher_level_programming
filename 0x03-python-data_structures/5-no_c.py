5d3612bc656aa2a145e8137f196d3b699d5b05fc
toshi-uy <s_borgia@hotmail.com>
2021-05-04 21:14:51 +0000

    changed pep8

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def no_c(my_string):
-         if my_string:
-                  new_string = ""
-                  for i in my_string:
-                           if i != 'C' and i != 'c':
-                                    new_string += i
-                  return new_string
+    if my_string:
+        new_string = ""
+        for i in my_string:
+            if i != 'C' and i != 'c':
+                new_string += i
+        return new_string
