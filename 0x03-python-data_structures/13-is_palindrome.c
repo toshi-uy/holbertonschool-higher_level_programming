32ea016d94f3e7046420947fbef8c4ab457493e4
toshi-uy <s_borgia@hotmail.com>
2021-05-05 00:05:52 +0000

    added missing libraries

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+
+/**
+ *is_palindrome - function in C that checks if a list is a palindrome.
+ *
+ *@head: double pointer to head
+ *Return: 0 if it is not a palindrome, 1 if it is a palindrome
+ */
+
+int is_palindrome(listint_t **head)
+{
+	listint_t *aux = *head;
+	int arr[1000];
+	int i = 0, j = 0;
+
+	if (!head)
+		return (0);
+	if (!*head || !(*head)->next)
+		return (1);
+	while (aux)
+	{
+		arr[i] = aux->n;
+		aux = aux->next;
+		i++;
+	}
+	while ( i > j)
+	{
+		if (arr[j] != arr[i - 1])
+			return (0);
+		j++;
+		i--;
+	}
+	return (1);
+}
