f0c2c93cd9a008074532461886840e29be3aed94
toshi-uy <s_borgia@hotmail.com>
2021-05-06 18:34:54 +0000

    added .format2

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
     for elem in sorted(a_dictionary.items()):
-        print(elem[0], ":", elem[1])
+        print("{}: {}".format(elem[0], elem[1]))
