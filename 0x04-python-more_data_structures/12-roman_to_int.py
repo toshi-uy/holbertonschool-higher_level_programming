bb730de3b49008b49c06d73c34fece9a79ad34b7
toshi-uy <s_borgia@hotmail.com>
2021-05-07 00:25:26 +0000

    fixed var name and border case

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,7 +2,7 @@
 def roman_to_int(roman_string):
     if roman_string is None or type(roman_string) is not str:
         return None
-    roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    rom = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     result = 0
     for i, c in enumerate(roman_string):
         if (i + 1) == len(roman_string) or rom[c] >= rom[roman_string[i + 1]]:
