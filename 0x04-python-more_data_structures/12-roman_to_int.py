03401ff16688161e4615bd5d1956a7a4b0520382
toshi-uy <s_borgia@hotmail.com>
2021-05-07 00:26:16 +0000

    changed return to 0

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     if roman_string is None or type(roman_string) is not str:
-        return None
+        return 0
     rom = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     result = 0
     for i, c in enumerate(roman_string):
