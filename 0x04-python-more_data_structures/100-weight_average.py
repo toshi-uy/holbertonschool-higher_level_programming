dadf84f5bdb809ca537d0dacf133c2ee8da112b2
toshi-uy <s_borgia@hotmail.com>
2021-05-07 02:18:31 +0000

    added final

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -3,6 +3,10 @@ def weight_average(my_list=[]):
         if my_list is None:
                 return 0
         result = []
+        div = 0
+        final = 1.0
         for item in my_list:
                 result.append(item[0] * item[1])
-        return result
+                div += item[1]
+        final = sum(result) / div
+        return final
