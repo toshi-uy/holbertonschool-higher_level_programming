a5308443f07c987e6660222fe13309e8db749566
toshi-uy <s_borgia@hotmail.com>
2021-05-07 02:32:58 +0000

    added file

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -1,12 +1,26 @@
-#include <Pylibrary.h>
+#include <Python.h>
 #include <stdio.h>
 
 void print_python_list(PyObject *p)
 {
-	printf("Hello World\n");
+	PyObject* repr = PyObject_Repr(p);
+	PyObject* str = PyUnicode_AsEncodedString(repr, "utf-8", "~E~");
+	const char *bytes = PyBytes_AS_STRING(str);
+
+	printf("REPR: %s\n", bytes);
+
+	Py_XDECREF(repr);
+	Py_XDECREF(str);
 }
 
 void print_python_bytes(PyObject *p)
 {
-        printf("Hello World\n");
+	PyObject* repr = PyObject_Repr(p);
+	PyObject* str = PyUnicode_AsEncodedString(repr, "utf-8", "~E~");
+	const char *bytes = PyBytes_AS_STRING(str);
+
+	printf("REPR: %s\n", bytes);
+
+	Py_XDECREF(repr);
+	Py_XDECREF(str);
 }
