334fd922a81ce27df8713b66750a6036118fa874
toshi-uy <s_borgia@hotmail.com>
2021-05-07 02:01:25 +0000

    fixed iguality

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
     for key, value in a_dictionary.items():
-        if value == value:
+        if a_dictionary[key] == value:
             del a_dictionary[key]
-            break
     return a_dictionary
