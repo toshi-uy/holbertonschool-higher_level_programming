ff46ac533fd65c4093ffa3b6cd427d13a874ddb9
toshi-uy <s_borgia@hotmail.com>
2021-05-07 02:25:57 +0000

    added file

---
+++ 0x04-python-more_data_structures/103-python.c
@@ -0,0 +1,12 @@
+#include <Pylibrary.h>
+#include <stdio.h>
+
+void print_python_list(PyObject *p)
+{
+	printf("Hello World\n");
+}
+
+void print_python_bytes(PyObject *p)
+{
+        printf("Hello World\n");
+}
