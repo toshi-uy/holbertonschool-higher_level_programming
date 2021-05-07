6821a7b5db0809b158da707b79e8a479e5e034e5
toshi-uy <s_borgia@hotmail.com>
2021-05-07 00:24:40 +0000

    fixed pepocho

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if roman_string is None or len(roman_string) == 0:
+    if roman_string is None or type(roman_string) is not str:
         return None
     roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     result = 0
