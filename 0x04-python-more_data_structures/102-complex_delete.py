90c0a6d19d4b31be9669d7a214a54ac1dacbec9a
toshi-uy <s_borgia@hotmail.com>
2021-05-07 01:59:59 +0000

    erased first if

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
-    if value is None:
-        return a_dictionary
     for key, value in a_dictionary.items():
         if value == value:
             del a_dictionary[key]
