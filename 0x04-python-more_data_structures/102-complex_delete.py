0fc9b3c798ca134955a6ca6dd011a5a3afdeaebb
toshi-uy <s_borgia@hotmail.com>
2021-05-07 02:02:52 +0000

    erased value from for and added .key

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
-    for key, value in a_dictionary.items():
+    for key in list(a_dictionary.keys()):
         if a_dictionary[key] == value:
             del a_dictionary[key]
     return a_dictionary
