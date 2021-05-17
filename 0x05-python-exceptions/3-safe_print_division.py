13b0ccb1d57e2a6b163bfd3417244140eee37bd0
toshi-uy <s_borgia@hotmail.com>
2021-05-17 17:15:16 +0000

    added return to case None

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -3,10 +3,11 @@ def safe_print_division(a, b):
     try:
         result = a / b
     except:
-        return None
+        result = None
     finally:
         if result is not None:
             print("Inside result: {}".format(result))
             return result
         else:
             print("Inside result: {}".format("None"))
+            return result
