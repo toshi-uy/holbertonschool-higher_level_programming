9de78c5540c104675e6cd4a8edaa0fad868bf153
toshi-uy <s_borgia@hotmail.com>
2021-05-17 17:13:32 +0000

    added print to None

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
-    result = 0
     try:
         result = a / b
     except:
@@ -9,3 +8,5 @@ def safe_print_division(a, b):
         if result is not None:
             print("Inside result: {}".format(result))
             return result
+        else:
+            print("Inside result: {}".format("None"))
