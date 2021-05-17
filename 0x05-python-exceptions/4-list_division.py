846c31eff131f69a88b749c7c9ed6085f0b9a53e
toshi-uy <s_borgia@hotmail.com>
2021-05-17 18:45:03 +0000

    forgot item = 0 in except

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -3,7 +3,7 @@ def list_division(my_list_1, my_list_2, list_length):
     div = []
     for i in range(list_length):
         try:
-            item =  my_list_1[i] / my_list_2[i]
+            item = my_list_1[i] / my_list_2[i]
         except (ZeroDivisionError):
             print("division by 0")
             item = 0
@@ -12,6 +12,7 @@ def list_division(my_list_1, my_list_2, list_length):
             print("wrong type")
         except (IndexError):
             print("Out of range")
+            item = 0
         finally:
             div.append(item)
     return div
