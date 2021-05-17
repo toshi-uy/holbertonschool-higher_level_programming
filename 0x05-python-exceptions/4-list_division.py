b0b5a2349287f8fb0d67848367e121323206e437
toshi-uy <s_borgia@hotmail.com>
2021-05-17 18:48:07 +0000

    don't know where is the error

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -7,11 +7,11 @@ def list_division(my_list_1, my_list_2, list_length):
         except (ZeroDivisionError):
             print("division by 0")
             item = 0
-        except (TypeError, ValueError):
-            item = 0
-            print("wrong type")
         except (IndexError):
             print("Out of range")
+            item = 0
+        except (TypeError, ValueError):
+            print("wrong type")
             item = 0
         finally:
             div.append(item)
