80128773a6c632d7b8997518343abbfd2074db9d
toshi-uy <s_borgia@hotmail.com>
2021-05-17 17:05:05 +0000

    added ValueError error

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -4,7 +4,7 @@ def safe_print_division(a, b):
     try:
         result = a / b
         return result
-    except (RuntimeError):
+    except (ValueError):
         return None
     finally:
         print("Inside result: {}".format(result))
