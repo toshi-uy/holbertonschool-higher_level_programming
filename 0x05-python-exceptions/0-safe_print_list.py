ef1d59802f95b184f540670bb9e2031112699c98
toshi-uy <s_borgia@hotmail.com>
2021-05-17 15:50:06 +0000

    added i = 0 case

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
+    i = 0
     try:
         for i in range(0, x):
             print("{}".format(my_list[i]), end="")
         print()
         return i + 1
     except:
-        print()
+        if i != 0:
+            print()
         return i
