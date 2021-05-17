eaf78a74923a537fcd04149d86420e27e882a073
toshi-uy <s_borgia@hotmail.com>
2021-05-17 18:43:24 +0000

    added value error

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -7,7 +7,7 @@ def list_division(my_list_1, my_list_2, list_length):
         except (ZeroDivisionError):
             print("division by 0")
             item = 0
-        except (TypeError):
+        except (TypeError, ValueError):
             item = 0
             print("wrong type")
         except (IndexError):
