74603731f51cadb097a1d75027204ed1bf5d5c7c
toshi-uy <s_borgia@hotmail.com>
2021-05-17 18:42:33 +0000

    added index error

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    div = []
+    for i in range(list_length):
+        try:
+            item =  my_list_1[i] / my_list_2[i]
+        except (ZeroDivisionError):
+            print("division by 0")
+            item = 0
+        except (TypeError):
+            item = 0
+            print("wrong type")
+        except (IndexError):
+            print("Out of range")
+        finally:
+            div.append(item)
+    return div
