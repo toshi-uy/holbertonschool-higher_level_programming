e3e79cc58d1c2a8e91c308e4808bd9ee5b95a809
toshi-uy <s_borgia@hotmail.com>
2021-05-17 16:31:29 +0000

    added x = 0 case

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -6,7 +6,8 @@ def safe_print_list_integers(my_list=[], x=0):
             print("{:d}".format(my_list[i]), end="")
             counter += 1
         except:
-            i += 1
+            if i != x:
+                i += 1
     if counter != 0:
         print()
     return counter
