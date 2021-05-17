43c368f87a8eb56568d6f4eee9a876eea2248edd
toshi-uy <s_borgia@hotmail.com>
2021-05-17 18:49:54 +0000

    changed a capital letter to verify

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -8,7 +8,7 @@ def list_division(my_list_1, my_list_2, list_length):
             print("division by 0")
             item = 0
         except (IndexError):
-            print("Out of range")
+            print("out of range")
             item = 0
         except (TypeError, ValueError):
             print("wrong type")
