10a1e61dea26505c96b9c4290a3483a151c0dbe9
toshi-uy <s_borgia@hotmail.com>
2021-05-18 21:48:37 +0000

    printitng position now

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -70,7 +70,11 @@ class Square:
         if self.__size == 0:
             print("")
         else:
+            for a in range(self.__position[1]):
+                print("")
             for i in range(self.__size):
+                for b in range(self.__position[0]):
+                    print(" ", end="")
                 for j in range(self.__size):
                     print("#", end="")
                 print("")
