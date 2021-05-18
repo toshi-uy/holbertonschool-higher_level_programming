01dabfeecf7c6d645235af785d3298d6cfcdcce1
toshi-uy <s_borgia@hotmail.com>
2021-05-18 21:03:24 +0000

    fixed spelling error and addded propietry

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python3
+"""defining a class Square"""
+
+
+class Square:
+    """ Classs that defines a square """
+    def __init__(self, size=0, position=(0, 0)):
+        """initialization method"""
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        if type(position) is not tuple:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if len(position) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if type(position[0]) is not int or type(position[1]) is not int:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if position[0] < 0 or position[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = position
+        self.__size = size
+
+    """Public instance method"""
+    def area(self):
+        """Return value of area"""
+        area = self.__size ** 2
+        return area
+
+    @property
+    def size(self):
+        """Return value of size"""
+        return self.__size
+
+    @property
+    def position(self):
+        """Return value of position"""
+        return self.__position
+
+    @size.setter
+    def size(self, number):
+        """initialization method"""
+        if type(number) is not int:
+            raise TypeError("size must be an integer")
+        if number < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = number
+
+    @position.setter
+    def position(self, value):
+        if type(position) is not tuple:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif len(position) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif type(position[0]) is not int or type(position[1]) is not int:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif position[0] < 0 or position[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        else:
+            self.__position = position
+
+    """Public instance method"""
+    def my_print(self):
+        if self.__size == 0:
+            print("")
+        else:
+            for i in range(self.__size):
+                for j in range(self.__size):
+                    print("#", end="")
+                print("")
