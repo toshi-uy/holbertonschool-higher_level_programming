8d71e48ecae73f2acf7960115cd7abb3c087776a
toshi-uy <s_borgia@hotmail.com>
2021-05-18 19:59:33 +0000

    added file and return area

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""defining a class Square"""
+
+
+class Square:
+    """ Classs that defines a square """
+    def __init__(self, size=0):
+        """initialization method"""
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+    def area(self):
+        """Return value of area"""
+        area = self.__size ** 2
+        return area
