99f060946e940f897348c580e8e884717090fe35
toshi-uy <s_borgia@hotmail.com>
2021-05-18 20:00:16 +0000

    added blank line to please pepocho

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -11,6 +11,7 @@ class Square:
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
+
     def area(self):
         """Return value of area"""
         area = self.__size ** 2
