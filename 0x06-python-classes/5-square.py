a0619a5913f81971d1132f2fa48c7b63ea35d369
toshi-uy <s_borgia@hotmail.com>
2021-05-18 20:37:47 +0000

    added file and removed extra print

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""defining a class Square"""
+
+
+class Square:
+    """ Classs that defines a square """
+    def __init__(self, size=0):
+        """initialization method"""
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    """Public instance method"""
+    def area(self):
+        """Return value of area"""
+        area = self.__size ** 2
+        return area
+
+    @property
+    def size(self):
+        """Return value of size"""
+        return self.__size
+
+    @size.setter
+    def size(self, number):
+        """initialization method"""
+        if type(number) is not int:
+            raise TypeError("size must be an integer")
+        if number < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = number
+
+    """Public instance method"""
+    def my_print(self):
+        if self.__size == 0:
+            print("")
+        else:
+            for i in range(self.__size):
+                for j in range(self.__size):
+                    print("#", end="")
+                print("")
