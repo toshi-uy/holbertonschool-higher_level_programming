551f13009ae963ae88d7a5203f26b2c47fb0589f
root <s_borgia@hotmail.com>
2021-05-21 18:18:42 +0000

    added 0 and testes

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Test Driven Doc import
+Don't import using __import__
+
+"""
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/0-add_integer.txt")
+
+def add_integer(a, b=98):
+    """
+    function that adds 2 integers
+    Returns a + b
+    a and b are first casted to integers if they are float
+    Raises:
+    TypeError: with the message a must be an integer if either
+    a or b are no integreres or floats
+    """
+    if isinstance(a, float): a = int(a)
+    if isinstance(b, float): b = int(b)
+    if type(a) != int:
+        raise TypeError("a must be an integrer")
+    if type(b) != int:
+        raise TypeError("b must be an integrer")
+    else:
+        return a + b
