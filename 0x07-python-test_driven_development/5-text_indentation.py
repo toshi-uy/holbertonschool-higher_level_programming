e51b6556fae6ab4edbb4760d4113d51965baf321
root <s_borgia@hotmail.com>
2021-05-21 20:37:00 +0000

    added 5 file

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+Test Driven Doc import
+Don't import using __import__
+"""
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/5-text_indentation.txt")
+
+def text_indentation(text):
+    """
+    function that prints a text with 2 new lines after each of
+    these characters: . ? and :
+    Args:
+    text: text to indent
+    """
+    if type(text) is not str:
+        raise TypeError("text must be a string")
+    space = 0
+    for a in text:
+        if space == 0:
+            if a == ' ':
+                continue
+            else:
+                space = 1
+        if space == 1:
+            if a == '?' or a == ':':
+                print(a)
+                print()
+                space = 0
+            elif a == '.':
+                print(a)
+                print()
+                space = 0
+            else:
+                print(a, end="")
