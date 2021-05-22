510dca4af93a4e859a105a0376fe3b06ff1641bf
root <s_borgia@hotmail.com>
2021-05-22 19:55:09 +0000

    added exception

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -12,4 +12,6 @@ def lazy_matrix_mul(m_a, m_b):
     function that multiplies 2 matrices by using the module NumPy
     Returns: new matrix
     """
+    if type(m_a) is str or type(m_b) is str:
+        raise TypeError("Scalar operands are not allowed, use '*' instead")
     return numpy.dot(m_a, m_b)
