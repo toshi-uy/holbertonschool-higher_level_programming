f9d76ab500dfc7af75c41638b3df31e3b7271624
root <s_borgia@hotmail.com>
2021-05-22 20:03:47 +0000

    added exception

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -14,4 +14,12 @@ def lazy_matrix_mul(m_a, m_b):
     """
     if type(m_a) is str or type(m_b) is str:
         raise TypeError("Scalar operands are not allowed, use '*' instead")
+    for i in m_a:
+        for j in i:
+            if type(j) is str:
+                raise TypeError("invalid data type for einsum")
+    for a in m_b:
+        for b in a:
+            if type(b) is str:
+                raise TypeError("invalid data type for einsum")
     return numpy.dot(m_a, m_b)
