42fd85600f95d1631ba1252120de4bbead8f1f6b
root <s_borgia@hotmail.com>
2021-05-22 03:14:57 +0000

    fixed integer

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -24,7 +24,7 @@ def add_integer(a, b=98):
     if type(b) is float:
         b = int(b)
     if type(a) != int:
-        raise TypeError("a must be an integrer")
+        raise TypeError("a must be an integer")
     if type(b) != int:
         raise TypeError("b must be an integrer")
     else:
