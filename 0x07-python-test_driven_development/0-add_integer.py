ecd249d0a4d676509d11817f4d8d05db40c95c49
root <s_borgia@hotmail.com>
2021-05-22 03:16:34 +0000

    fixed integer

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -26,6 +26,6 @@ def add_integer(a, b=98):
     if type(a) != int:
         raise TypeError("a must be an integer")
     if type(b) != int:
-        raise TypeError("b must be an integrer")
+        raise TypeError("b must be an integer")
     else:
         return a + b
