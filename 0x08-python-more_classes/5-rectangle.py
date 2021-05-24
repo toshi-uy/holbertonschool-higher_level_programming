e56b7b65d98fed03fbd5bee1b6aa13fc9e67621c
toshi-uy <s_borgia@hotmail.com>
2021-05-24 16:14:16 -0300

    fixed indent error and added 5 task

--- 0x08-python-more_classes/5-rectangle.py
+++ 0x08-python-more_classes/5-rectangle.py
@@ -87,3 +87,6 @@ class Rectangle:
             The representation of the rectangle.
         """
         return ('Rectangle({},{})'.format(self.__width, self.__height))
+
+    def __del__(self):
+        print("Bye rectangle...")
