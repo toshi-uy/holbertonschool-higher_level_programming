b4581f905d4c1c29d9d610aaae6bef352ab4cdb0
toshi-uy <s_borgia@hotmail.com>
2021-05-24 18:48:21 -0300

    trying fix on pep8

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -77,6 +77,6 @@ class Rectangle:
         """
         rect = ""
         if self.__height != 0 and self.__width != 0:
-            rect += "\n".join(("#" * self.__width) \
-for j in range(self.__height))
+            rect += "\n".join(("#" * self.__width) 
+                                            for j in range(self.__height))
         return rect
