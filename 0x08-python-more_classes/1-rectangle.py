632a6b866778be0101fa66ac7bf0e3f3a2258b52
toshi-uy <s_borgia@hotmail.com>
2021-05-24 14:54:01 -0300

    fixed parameters comments

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -2,23 +2,22 @@
 
 
 class Rectangle:
-    """ Rectangle Class
-    Attributes:
-        attr1 (str): Description of `attr1`.
+    """ 
+    Rectangle Class
     """
-    def __init__(self, width=0, height=0):
-        """docstring on the __init__ method.
-        Args:
-            param1 (str): Description of `param1`.
-            param2 (:obj:`int`, optional): Description of `param2`. Multiple
-                lines are supported.
-            param3 (:obj:`list` of :obj:`str`): Description of `param3`.
 
+
+    def __init__(self, width=0, height=0):
         """
+        docstring on the __init__ method.
+        Args:
+            width (int): Rectangle width.
+            heigth (int): Rectangle height
+        """
+        self.__height = height
         self.__width = width
-        self.__heigth = height
+
     @property
-    """str: Properties should be documented in their getter method."""
     def width(self):
         return self.__width
 
@@ -32,15 +31,14 @@ class Rectangle:
             raise ValueError("width must be >= 0")
     
     @property   
-    """str: Properties should be documented in their getter method."""
     def height(self):
-        return self.__heigth
+        return self.__height
 
     @height.setter
     def height(self, value):
         if type(value) is int and value > 0:
-            self.__width = value
+            self.__height = value
         elif type(value) is not int:
-            raise TypeError("width must be an integer")
+            raise TypeError("height must be an integer")
         elif value < 0:
-            raise ValueError("width must be >= 0")
+            raise ValueError("height must be >= 0")
