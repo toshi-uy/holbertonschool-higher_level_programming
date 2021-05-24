4c3ee3ab6ab79c8d4aca25a9fd38d879c3103c60
toshi-uy <s_borgia@hotmail.com>
2021-05-24 19:54:06 -0300

    fixing pep8

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -12,7 +12,6 @@ class Rectangle:
     number_of_instances = 0
     print_symbol = "#"
 
-
     def __init__(self, width=0, height=0):
         """
         docstring on the __init__ method.
@@ -92,8 +91,8 @@ class Rectangle:
         """
         r = ""
         if self.__height != 0 and self.__width != 0:
-            r += '\n'.join((str(self.print_symbol) * self.__width) 
-                            for j in range(self.__height))
+            r += '\n'.join((str(self.print_symbol) *
+                                self.__width) for j in range(self.__height))
         return r
 
     def __repr__(self):
