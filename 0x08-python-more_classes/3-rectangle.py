d53cc467e9faad095fc1979d776ca64a790faa3b
toshi-uy <s_borgia@hotmail.com>
2021-05-24 17:26:39 -0300

    changed logic to print one \n less

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -78,4 +78,9 @@ class Rectangle:
         if self.__height == 0 or self.__width == 0:
             return 0
         else:
-            return ((("#" * self.__width) + '\n') * self.__height)
+            for j in range(self.__height):
+                for i in range(self.__width):
+                    rect += '#'
+                    if i < self.__width:
+                        rect += '\n'
+            return rect
