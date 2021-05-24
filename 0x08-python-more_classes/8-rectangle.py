5fe127c52787ce399a7e5c91715da1ad83845e20
toshi-uy <s_borgia@hotmail.com>
2021-05-24 20:07:21 -0300

    fixing pep8

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -106,10 +106,10 @@ class Rectangle:
     def __del__(self):
         Rectangle.number_of_instances -= 1
         print("Bye rectangle...")
-    
+
     @staticmethod
     def bigger_or_equal(rect_1, rect_2):
-        """static method that returns the biggest rectangle based on the area"""
+        """staticmethod that return the biggest rectangle based on the area"""
         if not isinstance(rect_1, Rectangle):
             raise TypeError("rect_1 must be an instance of Rectangle")
         if not isinstance(rect_2, Rectangle):
