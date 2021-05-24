dddf4cc7efb024885eb7cf0c4a7380bbcab2a800
toshi-uy <s_borgia@hotmail.com>
2021-05-24 17:56:38 -0300

    fixed all

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -97,7 +97,7 @@ class Rectangle:
         Returns:
             The representation of the rectangle.
         """
-        return ('Rectangle({},{})'.format(self.__width, self.__height))
+        return ('Rectangle({}, {})'.format(self.__width, self.__height))
 
     def __del__(self):
         Rectangle.number_of_instances -= 1
