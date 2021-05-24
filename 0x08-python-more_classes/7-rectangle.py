3d624b755ff8718e577d29e8f0d23f4aa5df7149
toshi-uy <s_borgia@hotmail.com>
2021-05-24 19:59:12 -0300

    fixing pep8

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -91,7 +91,7 @@ class Rectangle:
         """
         r = ""
         if self.__height != 0 and self.__width != 0:
-            r += '\n'.join((str(self.print_symbol)* 
+            r += '\n'.join((str(self.print_symbol) *
                             self.__width) for j in range(self.__height))
         return r
 
