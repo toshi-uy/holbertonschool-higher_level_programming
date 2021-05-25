c14daaa3fec22e0783cebfdbfff4074bb8fdf046
toshi-uy <s_borgia@hotmail.com>
2021-05-25 16:47:10 -0300

    fixing pep8

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
 """Class that accepts only first_name variables"""
 
+
 class Frozen(object):
+    """Frozen class attributes"""
     __List = []
+
     def __setattr__(self, key, value):
+        """setting the values to check attributes"""
         setIsOK = False
         for item in self.__List:
             if key == item:
@@ -12,9 +16,12 @@ class Frozen(object):
         if setIsOK == True:
             object.__setattr__(self, key, value)
         else:
-            raise AttributeError("%r object has no attribute %r" % (self.__class__.__name__, key))
+            raise AttributeError("%r object has no attribute %r"
+                                  % (self.__class__.__name__, key))
 
 class LockedClass(Frozen):
+    """Locked Class using the Class Frozen"""
     _Frozen__List = ["first_name"]
     def __init__(self):
+        """setting the self first name attribute"""
         self.first_name = ""
