ee69f3e8759cba74fd5c6c9419a9547114245b5c
toshi-uy <s_borgia@hotmail.com>
2021-05-25 10:52:11 -0300

    testing print

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -39,4 +39,4 @@ def solve(n):
                        if not under_attack(i+1, solution)]
     return solutions
  
-for answer in solve(BOARD_SIZE): print(list(enumerate(answer, start=1)))
+for answer in solve(BOARD_SIZE): print((enumerate(answer, start=0)))
