5f55f9cf54ccd8231b9139bfdf9e50aae4a2b1b2
toshi-uy <s_borgia@hotmail.com>
2021-05-25 10:50:19 -0300

    changed logic

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -24,42 +24,19 @@ elif N < 4:
     print("N must be at least 4")
     exit(1)
 
-"""N x N matrix with all elements 0"""
-board = [[0] * N for i in range(N)]
-
-def is_attack(i, j):
-    """checking if there is a queen in row or column"""
-    for k in range(0, N):
-        if board[i][k] == 1 or board[k][j] == 1:
-            return True
-    """checking diagonals"""
-    for k in range(0, N):
-        for l in range(0, N):
-            if (k + l == i + j) or (k - l == i - j):
-                if board[k][l] == 1:
-                    return True
-    return False
-
-def N_queen(n):
-    """if n is 0, solution found"""
-    if n == 0:
-        return True
-    for i in range(0, N):
-        for j in range(0, N):
-            """checking if we can place a queen here or not
-            queen will not be placed if the place is being attacked
-            or already occupied"""
-            if (not(is_attack(i, j))) and (board[i][j] != 1):
-                board[i][j] = 1
-                """recursion"""
-                """wether we can put the next queen with this arrangment or not"""
-                if N_queen(n - 1) == True:
-                    return True
-                board[i][j] = 0
-
-    return False
-
-N_queen(N)
-for i in range(0, N):
-        for j in range(0, N):
-            print(board)
+BOARD_SIZE = N
+ 
+def under_attack(col, queens):
+    return col in queens or \
+           any(abs(col - x) == len(queens)-i for i,x in enumerate(queens))
+ 
+def solve(n):
+    solutions = [[]]
+    for row in range(n):
+        solutions = [solution+[i+1]
+                       for solution in solutions
+                       for i in range(BOARD_SIZE)
+                       if not under_attack(i+1, solution)]
+    return solutions
+ 
+for answer in solve(BOARD_SIZE): print(list(enumerate(answer, start=1)))
