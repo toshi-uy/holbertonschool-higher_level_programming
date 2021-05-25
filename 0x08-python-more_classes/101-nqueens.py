068f96867bfe2f1100666318bad809dd1864e91a
toshi-uy <s_borgia@hotmail.com>
2021-05-25 10:54:42 -0300

    testing print

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -28,15 +28,16 @@ BOARD_SIZE = N
  
 def under_attack(col, queens):
     return col in queens or \
-           any(abs(col - x) == len(queens)-i for i,x in enumerate(queens))
+           any(abs(col - x) == len(queens) - i for i,x in enumerate(queens))
  
 def solve(n):
     solutions = [[]]
     for row in range(n):
-        solutions = [solution+[i+1]
+        solutions = [solution+[i + 1]
                        for solution in solutions
                        for i in range(BOARD_SIZE)
-                       if not under_attack(i+1, solution)]
+                       if not under_attack(i + 1, solution)]
     return solutions
- 
+
+print(solve(BOARD_SIZE))
 for answer in solve(BOARD_SIZE): print(list(enumerate(answer, start=0)))
