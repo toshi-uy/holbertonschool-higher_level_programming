aa278b0682d4ae66596a8011f1d1b116b0637cf5
root <s_borgia@hotmail.com>
2021-05-26 17:02:04 +0000

    removed int casting

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -16,10 +16,10 @@ import sys
 if len(sys.argv) != 2:
     print("Usage: nqueens N")
     exit(1)
-N = sys.argv[1]
-if type(N) is not int:
+if type(sys.argv[1]) is not int:
     print("N must be a number")
     exit(1)
+N = int(sys.argv[1])
 elif N < 4:
     print("N must be at least 4")
     exit(1)
