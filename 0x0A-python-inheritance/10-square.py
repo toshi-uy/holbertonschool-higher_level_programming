d38882e303a85f8f5d65e8a2ffac3dddd21752fe
toshi-uy <s_borgia@hotmail.com>
2021-06-01 20:35:54 -0300

    added 10 task and fixed error in 9 and 8

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""class Square"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+	"""
+	Square class that inherits from Rectangle, that inherits
+	from BaseGeometry
+	"""
+	def __init__(self, size):
+		"""Instantiation with size"""
+		self.integer_validator("size", size)
+		self.__size = size
+	
+	def area(self):
+		"""area method"""
+		return self.__size **2
--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""class BaseGeometry"""
+"""class Rectangle"""
 
 
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
