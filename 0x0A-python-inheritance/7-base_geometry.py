b422bf9ad8a81f88b5bf3bc910bf46eef4fadc4a
toshi-uy <s_borgia@hotmail.com>
2021-06-01 19:46:31 -0300

    added test file and name format

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -15,6 +15,6 @@ class BaseGeometry:
     def integer_validator(self, name, value):
         """Public instance method that validates value"""
         if type(value) is not int:
-            raise TypeError("<name> must be an integer")
+            raise TypeError("{} must be an integer".format(name))
         if value < 0:
-            raise ValueError("<name> must be greater than 0")
+            raise ValueError("{} must be greater than 0".format(name))
