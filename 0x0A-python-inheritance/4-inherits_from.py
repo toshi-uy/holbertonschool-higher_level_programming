6cd6ddebea6c48fef74a58b91bcda825dd968842
toshi-uy <s_borgia@hotmail.com>
2021-06-01 19:08:38 -0300

    added function

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+"""
+Function that returns True if the object is an instance of a class that
+inherited (directly or indirectly) from the specified class ; otherwise False.
+"""
+
+
+def inherits_from(obj, a_class):
+    """sub instance of a class"""
+    return (issubclass(obj, a_class))
