11f7c0eb1ee553d0e7d0b8309f2efb331fda4a67
toshi-uy <s_borgia@hotmail.com>
2021-06-01 20:19:49 -0300

    added 8 task

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""class BaseGeometry"""
+
+
+class BaseGeometry:
+    """
+    empty class BaseGeometry
+    Args:
+    """
+
+    def area(self):
+        """area module"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Public instance method that validates value"""
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+class Rectangle(BaseGeometry):
+    """
+    Rectangle class that inherits from BaseGeometry
+    """
+    def __init__(self, width, height):
+        """inicialization for class"""
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
