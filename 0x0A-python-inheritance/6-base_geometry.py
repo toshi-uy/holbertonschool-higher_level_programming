b5e41a2399421c2246863d86bf54b570e4541d6a
root <s_borgia@hotmail.com>
2021-06-01 22:36:50 +0000

    fixed name

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""class BaseGeometry"""
+
+
+class BaseGeometry:
+    """
+    empty class BaseGeometry
+    Args:
+    """
+
+    def area(self):
+        """area module"""
+        raise Exception("area() is not implemented")
