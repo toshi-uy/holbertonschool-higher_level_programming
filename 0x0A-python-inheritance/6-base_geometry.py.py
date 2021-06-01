595b01f1baf33b49f721589b07b62c57b729feb0
toshi-uy <s_borgia@hotmail.com>
2021-06-01 19:33:59 -0300

    added task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""class BaseGeometry"""
+
+
+class BaseGeometry:
+    """
+    empty class BaseGeometry
+    Args:
+    """
+    
+    def area(self):
+        raise Exception("area() is not implemented")
