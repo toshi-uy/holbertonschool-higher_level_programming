7518af2cb84861748af3a8c1cbe8aa4de748b405
toshi-uy <s_borgia@hotmail.com>
2021-06-01 21:05:09 -0300

    added hasattr to check if it's on dict

---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""function that adds a new attribute to an object if it’s possible"""
+
+def add_attribute(obj, name, value):
+    """Adds a new attribute"""
+    if not hasattr(obj, "__dict__"):
+        raise TypeError("can't add new attribute")
+    else:
+        setattr(obj, name, value)
