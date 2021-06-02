eaa211b03d5559e9d070cbe0016d738f308a6a58
toshi-uy <s_borgia@hotmail.com>
2021-06-02 19:12:48 -0300

    added load from json

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,20 @@
+#!/urs/bin/python3
+""" Task 7 """
+import json
+import sys
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+
+def load_from_json_file(filename):
+    """
+    script that adds all arguments to a Python list,
+    and then save them to a file
+    """
+    try:
+        js_list = load_from_json_file("add_item.json")
+    except:
+        js_list = []
+    for i in range(1, len(sys.argv)):
+        js_list.append(sys.argv)
+    save_to_json_file(js_list, "add_item.json")
