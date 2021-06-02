55dbcf67b1614e19fa73249d972bc6eba9c73e68
toshi-uy <s_borgia@hotmail.com>
2021-06-02 18:19:33 -0300

    added mode

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -4,5 +4,5 @@ import json
 
 
 def save_to_json_file(my_obj, filename):
-    with open(filename, 'w', encoding='utf-8') as f:
+    with open(filename, mode='w', encoding='utf-8') as f:
         f.write(json.dumps(my_obj))
