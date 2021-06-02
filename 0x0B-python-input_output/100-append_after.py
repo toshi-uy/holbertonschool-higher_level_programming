37522d89d67e9f785f4cd6602d30f9430c39e58b
toshi-uy <s_borgia@hotmail.com>
2021-06-02 20:48:44 -0300

    removed trailing whitespace

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -7,7 +7,7 @@ def append_after(filename="", search_string="", new_string=""):
     function that inserts a line of text to a file,
     after each line containing a specific string
     """
-    text = open(filename, 'r').readlines() 
+    text = open(filename, 'r').readlines()
     with open(filename, 'w') as f:
         for line in text:
             f.write(line)
