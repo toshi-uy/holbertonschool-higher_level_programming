d0d35829039f2f3380c6af5f95015f53be035ebe
toshi-uy <s_borgia@hotmail.com>
2021-06-02 18:18:05 -0300

    removed +

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -4,5 +4,5 @@ import json
 
 
 def save_to_json_file(my_obj, filename):
-    with open(filename, 'w+', encoding='utf-8') as f:
+    with open(filename, 'w', encoding='utf-8') as f:
         f.write(json.dump(my_obj, f))
