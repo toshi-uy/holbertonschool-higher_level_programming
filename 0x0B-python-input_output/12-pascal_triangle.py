3ab33fdd94a742401ab8ce53754a6b8858e5a98f
toshi-uy <s_borgia@hotmail.com>
2021-06-02 20:20:40 -0300

    changed methond

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -10,7 +10,9 @@ def pascal_triangle(n):
     trow = []
     if n <= 0:
         return trow
-    y = [0]
-    for x in range(n):
-        trow=[left + right for left,right in zip(trow + y, y + trow)]
+    for line in range(1, n + 1):
+    c = 1
+    for i in range(1, line + 1):
+        trow.append(c)
+        c = int(c * (line - i) / i)
     return trow
