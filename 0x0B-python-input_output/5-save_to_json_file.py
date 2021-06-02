a609f58ea848cfb2f7157ac516b1037823bb4457
toshi-uy <s_borgia@hotmail.com>
2021-06-02 18:12:30 -0300

    added w

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -2,6 +2,7 @@
 """ Task 5 """
 import json
 
+
 def save_to_json_file(my_obj, filename):
-    with json.dump(my_obj, filename) as f:
-        return f
+    with open(filename, 'w') as f:
+        return json.dump(my_obj, f)
