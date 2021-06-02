b5bb1d01ed5ac62ae9c7c15152a580a7b0b0000b
toshi-uy <s_borgia@hotmail.com>
2021-06-02 19:25:33 -0300

    removed wrong def

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,20 +1,17 @@
 #!/usr/bin/python3
-""" Task 7 """
-import json
+"""
+script that adds all arguments to a Python list,
+and then save them to a file
+"""
 import sys
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
-def load_from_json_file(filename):
-    """
-    script that adds all arguments to a Python list,
-    and then save them to a file
-    """
-    try:
-        js_list = load_from_json_file("add_item.json")
-    except:
-        js_list = []
-    for i in sys.argv[1:]:
-        js_list.append(i)
-        save_to_json_file(js_list, "add_item.json")
+try:
+    js_list = load_from_json_file("add_item.json")
+except:
+    js_list = []
+for i in sys.argv[1:]:
+    js_list.append(i)
+save_to_json_file(js_list, "add_item.json")
