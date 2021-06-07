0ac42386c048898583336618736facacf1ea235e
toshi-uy <s_borgia@hotmail.com>
2021-06-07 13:16:12 -0300

    removed "."

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -7,10 +7,10 @@ class Square(Rectangle):
     """ class Square """
     def __init__(self, size, x=0, y=0, id=None):
         """ Constructor for Square """
-        super().Rectangle.__init__(size, size, x, y, id)
+        super().__init__(size, size, x, y, id)
         self.size = size
 
     def __str__(self):
-        return ("[Square] (<{}>) <{}>/<{}> - <{}>".format(self.id, self._Rectangle.__x,
-                                                          self._Rectangle.__y,
-                                                          self._Rectangle.__width))
+        return ("[Square] (<{}>) <{}>/<{}> - <{}>"
+                .format(self.id, self._Rectangle__x, self._Rectangle__y,
+                        self._Rectangle__width))
