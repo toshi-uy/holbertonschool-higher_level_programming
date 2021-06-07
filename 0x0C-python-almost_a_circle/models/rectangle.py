f7b5f540857abdbbf3a8b6590af3e63531f7c002
toshi-uy <s_borgia@hotmail.com>
2021-06-07 13:54:54 -0300

    demoved =

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -94,13 +94,13 @@ class Rectangle(Base):
         """ assigns an argument to each attribute """
         if len(args) != 0:
             self.id = args[0]
-            if len(args) >= 1:
+            if len(args) > 1:
                 self.__width = args[1]
-            if len(args) >= 2:
+            if len(args) > 2:
                 self.__height = args[2]
-            if len(args) >= 3:
+            if len(args) > 3:
                 self.__x = args[3]
-            if len(args) >= 4:
+            if len(args) > 4:
                 self.__y = args[4]
         else:
             if kwargs is not None:
