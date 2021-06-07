72eadb8ef1d9d0c14eba004f151fb4d1a5eead10
toshi-uy <s_borgia@hotmail.com>
2021-06-07 11:22:24 -0300

    0 to 5

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -91,12 +91,12 @@ class Rectangle(Base):
     def update(self, *args):
         """ assigns an argument to each attribute """
         if len(args) != 0:
-            self.id = args[1]
+            self.id = args[0]
             if len(args) > 1:
-                self.__width = args[2]
+                self.__width = args[1]
             if len(args) > 2:
-                self.__height = args[3]
+                self.__height = args[2]
             if len(args) > 3:
-                self.__x = args[4]
+                self.__x = args[3]
             if len(args) > 4:
-                self.__y = args[5]
+                self.__y = args[4]
