813312c86089f48257f98c6788aeba6b9e9e6413
toshi-uy <s_borgia@hotmail.com>
2021-06-08 15:21:24 -0300

    added int casting

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -112,8 +112,8 @@ class Base:
                 if cls.__name__ == "Rectangle":
                     for row in reader:
                         dict_cvs = {
-                            "id": row[0], "width": row[1], "height": row[2],
-                            "x": row[3], "y": row[4]
+                            "id": int(row[0]), "width": int(row[1]), "height": int(row[2]),
+                            "x": int(row[3]), "y": int(row[4])
                         }
                         obj = cls.create(**dict_cvs)
                         csv_list.append(obj)
