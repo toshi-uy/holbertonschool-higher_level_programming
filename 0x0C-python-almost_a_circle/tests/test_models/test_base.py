a073bd875d1f5190971eb76137374fb28da409eb
toshi-uy <s_borgia@hotmail.com>
2021-06-08 19:56:26 -0300

    changed things to please pep8

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -14,7 +14,8 @@ class Test_pep8(unittest.TestCase):
     def test_pep8_conformance(self):
         """Test that we conform to PEP8."""
         pep8style = pep8.StyleGuide(quiet=True)
-        result = pep8style.check_files(['tests/test_models/test_base.py'])
+        result = pep8style.check_files(['tests/test_models/test_base.py',
+                                        'models/base.py'])
         self.assertEqual(result.total_errors, 0,
                          "Found code style errors (and warnings).")
 
@@ -50,17 +51,20 @@ class Test_Base_Model(unittest.TestCase):
         b = Base([12])
         self.assertEqual(b.id, [12])
         """testing long list as id"""
-        b = Base([1,2,3,4])
-        self.assertEqual(b.id, [1,2,3,4])
+        b = Base([1, 2, 3, 4])
+        self.assertEqual(b.id, [1, 2, 3, 4])
         """testing empty list as id"""
+        b = Base([])
         self.assertEqual(b.id, [])
 
     def test_string_as_id(self):
         """testing string as id"""
+        b = Base(12)
         self.assertEqual(b.id, "hola")
 
     def test_True(self):
         """testing True"""
+        b = Base(12)
         self.assertEqual(b.id, True)
 
     def test_accesing_nb(self):
@@ -68,12 +72,12 @@ class Test_Base_Model(unittest.TestCase):
         b = Base(10)
         with self.assertRaises(AttributeError):
             print(self.nb_objects)
+
     def test_accesing__nb(self):
         """testing trying to access and print the __nb"""
         b = Base(10)
         with self.assertRaises(AttributeError):
             print(b.__nb_objects)
-
 
 if __name__ == '__main__':
     unittest.main()
