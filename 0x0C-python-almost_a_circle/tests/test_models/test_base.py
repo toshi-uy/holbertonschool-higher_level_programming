7ce1e501b0f3d156f5bed4fc615854d4f2bb24ea
toshi-uy <s_borgia@hotmail.com>
2021-06-08 16:38:51 -0300

    adding test cases

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -9,4 +9,29 @@ from models.base import Base
 class Test_Base_Model(unittest.TestCase):
     """test cases for base model"""
 
-    def test_
+    def test_correctbase(self):
+        """testing base with correct output"""
+        b = Base(85)
+        self.assertEqual(b.id, 85)
+        b = Base(100)
+        self.assertEqual(b.id, 100)
+        """negative numbers"""
+        b = Base(-45)
+        self.assertEqual(b.id, -45)
+        """None as id"""
+        b = Base()
+        self.assertEqual(b.id, 1)
+        """Other None to test id increment"""
+        b = Base()
+        self.assertEqual(b.id, 2)
+        """Other with text None to test function"""
+        b = Base(None)
+        self.assertEqual(b.id, 3)
+
+    def test_too_many_args(self):
+        """testing too many args"""
+        with self.assertRaises(TypeError):
+            b = Base(1, 2)
+
+if __name__ == '__main__':
+    unittest.main()
