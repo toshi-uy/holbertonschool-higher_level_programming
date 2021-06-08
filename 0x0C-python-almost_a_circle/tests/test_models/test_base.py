c6a5b5468549f8c7ced5e712cacd34e1a1b5b153
toshi-uy <s_borgia@hotmail.com>
2021-06-08 16:15:24 -0300

    added test model base and 'r' to csv open

---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Unittest for base model
+"""
+import unittest
+from models.base import Base
+
+
+class Test_Base_Model(unittest.TestCase):
+    """test cases for base model"""
+
+    def test_
