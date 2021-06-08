4ba3b05513b839f8238f54ab40c846a29af0e863
toshi-uy <s_borgia@hotmail.com>
2021-06-08 20:49:49 -0300

    added various tests for rectangle

---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+"""
+Unittest for Rectangle model
+"""
+import unittest
+import pep8
+import io
+import contextlib
+import json
+import inspect
+from models.base import Base
+from models.rectangle import Rectangle
+
+
+class Test_pep8(unittest.TestCase):
+    """pep8 test cases class"""
+    def test_pep8_conformance(self):
+        """Test that we conform to PEP8."""
+        pep8style = pep8.StyleGuide(quiet=True)
+        result = pep8style.check_files(['tests/test_models/test_rectangle.py',
+                                        'models/rectangle.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
+
+
+class TestBaseDocs(unittest.TestCase):
+    """Base document tests"""
+
+    @classmethod
+    def setUpClass(cls):
+        """Testing class"""
+        cls.rect_funcs = inspect.getmembers(Rectangle, inspect.isfunction)
+
+    def test_module_docstring(self):
+        """module docstring length"""
+        self.assertTrue(len(Rectangle.__doc__) >= 1)
+
+    def test_class_docstring(self):
+        """Class docstring length"""
+        self.assertTrue(len(Rectangle.__doc__) >= 1)
+
+    def test_inheritance(self):
+        """Testing inheritance"""
+        Rectangle.reset_objects()
+        self.assertEqual(issubclass(Rectangle, Base), True)
+
+    def test_object(self):
+        """Testing object inheritance"""
+        Rectangle.reset_objects()
+        r1 = Rectangle(1, 1)
+        self.assertEqual(isinstance(r1, Rectangle), True)
+
+class Test_Rectangle(unittest.TestCase):
+    """test cases for rectangle"""
