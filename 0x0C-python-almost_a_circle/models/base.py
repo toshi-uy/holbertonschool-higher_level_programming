7e78c8c0c741a1ca7c20559cdf469f47becc6237
toshi-uy <s_borgia@hotmail.com>
2021-06-08 14:59:04 -0300

    added square

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -111,10 +111,17 @@ class Base:
                 reader = csv.reader(f)
                 if cls.__name__ == "Rectangle":
                     fields = ["id", "width", "height", "x", "y"]
-                dict_cvs = csv.DictReader(reader, fieldnames=fields)
-                csv_list.append(dict_cvs)
-                obj = cls.create(**dict_cvs)
-                csv_list.append(obj)
-                return csv_list
+                    dict_cvs = csv.DictReader(reader, fieldnames=fields)
+                    csv_list.append(dict_cvs)
+                    obj = cls.create(**dict_cvs)
+                    csv_list.append(obj)
+                    return csv_list
+                elif cls.__name__ == "Square":
+                    fields = ["id", "size", "x", "y"]
+                    dict_cvs = csv.DictReader(reader, fieldnames=fields)
+                    csv_list.append(dict_cvs)
+                    obj = cls.create(**dict_cvs)
+                    csv_list.append(obj)
+                    return csv_list
         except:
             return csv_list
