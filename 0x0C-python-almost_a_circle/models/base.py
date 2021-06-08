1d78492f3205a0db4122ca538671d7df5b499469
toshi-uy <s_borgia@hotmail.com>
2021-06-08 13:48:57 -0300

    removed base from else

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -51,7 +51,6 @@ class Base:
             if list_objs is not None:
                 for a in list_objs:
                     jason.append(cls.to_dictionary(a))
-                jason = Base.to_json_string(jason)
                 json.dump(jason, f)
             else:
                 json.dump(jason, f)
