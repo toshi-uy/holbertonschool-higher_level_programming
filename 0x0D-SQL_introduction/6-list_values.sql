75be8bd6975391bce2f934b816a297dc9e95a91f
toshi-uy <s_borgia@hotmail.com>
2021-07-12 17:05:55 -0300

    changed to select

--- 0x0D-SQL_introduction/6-list_values.sql
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -1,3 +1,3 @@
 -- script that lists all rows of the table first_table from the database of your MySQL server.
 -- The database name will be passed as argument of mysql
-SHOW rows FROM `first_table`;
+SELECT * FROM first_table;
