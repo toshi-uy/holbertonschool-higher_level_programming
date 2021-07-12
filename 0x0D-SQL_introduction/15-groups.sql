bd357150285b1f0afb78fb6b4989913d143ea013
toshi-uy <s_borgia@hotmail.com>
2021-07-12 18:16:51 -0300

    removed first score

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -3,4 +3,4 @@
 -- The result column name should be average
 -- The database name will be passed as an argument of the mysql command
 
-SELECT score GROUP BY `score` AS `number` FROM second_table ORDER BY score DESC;
+SELECT GROUP BY `score` AS `number` FROM second_table ORDER BY score DESC;
