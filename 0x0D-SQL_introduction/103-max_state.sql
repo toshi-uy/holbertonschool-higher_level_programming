83705f9d2b0e14efe169089e57a622895855698f
toshi-uy <s_borgia@hotmail.com>
2021-07-12 20:27:05 -0300

    orderded by max temp

--- 0x0D-SQL_introduction/103-max_state.sql
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -1,3 +1,3 @@
 -- Write a script that displays the average temperature (Fahrenheit)
 -- by city ordered by temperature (descending).
-SELECT state, MAX(value) AS max_temp FROM temperatures GROUP BY state ORDER BY state DESC;
+SELECT state, MAX(value) AS max_temp FROM temperatures GROUP BY state ORDER BY max_temp DESC;
