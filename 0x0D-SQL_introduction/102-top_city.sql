a914f1ac8e5b5b7d704c8b7d148b496ce00477ae
toshi-uy <s_borgia@hotmail.com>
2021-07-12 20:22:54 -0300

    limit instead of head

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,3 +1,3 @@
 -- Write a script that displays the average temperature (Fahrenheit)
 -- by city ordered by temperature (descending).
-SELECT city, AVG(value) AS avg_temp FROM temperatures WHERE month BETWEEN 7 and 8 GROUP BY city ORDER BY avg_temp DESC HEAD 3;
+SELECT city, AVG(value) AS avg_temp FROM temperatures WHERE month BETWEEN 7 and 8 GROUP BY city ORDER BY avg_temp DESC LIMIT 3;
