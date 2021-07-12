bc93ccb88954cb81760ba8d59ba65c5b1f621e2f
toshi-uy <s_borgia@hotmail.com>
2021-07-12 16:40:38 -0300

    create table test

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,3 +1,3 @@
 -- script that list full details of table of your MySQL server.
 -- The database name will be passed as argument of mysql
-SHOW TABLES FULL FROM first_table;
+SHOW CREATE TABLES first_table;
