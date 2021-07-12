af0a25a5e8f32c3dbd183077c63ad9333bca903b
toshi-uy <s_borgia@hotmail.com>
2021-07-12 18:15:18 -0300

    testing group by

---
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -0,0 +1,6 @@
+-- script that computes the score average of all records in the
+-- table second_table of the database hbtn_0c_0 in your MySQL server.
+-- The result column name should be average
+-- The database name will be passed as an argument of the mysql command
+
+SELECT score GROUP BY score AS `number` FROM second_table ORDER BY score DESC;
