9c54844b60177ca18dad19b5a2a2f1c5a3070af7
toshi-uy <s_borgia@hotmail.com>
2021-07-13 15:49:37 -0300

    adding () testing two columns

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -4,4 +4,4 @@
 -- You are not allowed to use the JOIN keyword
 -- The database name will be passed as an argument of the mysql command
 
-SELECT DISTINCT name = California FROM cities, states ORDER BY cities.id ASC;
+SELECT id, name WHERE state_id IN(SELECT FROM id FROM states WHERE name="California") ORDER BY id ASC;
