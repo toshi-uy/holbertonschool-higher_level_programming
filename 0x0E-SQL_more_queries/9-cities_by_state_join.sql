102d6abb28fee8c0727daface0915daffa316e18
toshi-uy <s_borgia@hotmail.com>
2021-07-13 16:31:33 -0300

    FULL OUTER IS BETTER

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -5,5 +5,5 @@
 -- The database name will be passed as an argument of the mysql command
 
 SELECT cities.id, cities.name FROM cities
-A INNER JOIN states ON cities.names = states.names
+FULL OUTER JOIN states ON cities.names = states.names
 ORDER BY cities.id ASC;
