8f81edb024c872367de394834078546d14c421b4
toshi-uy <s_borgia@hotmail.com>
2021-07-13 15:05:03 -0300

    changed to usage

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -7,4 +7,5 @@
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
 SET PASSWORD FOR  'user_0d_2'@'localhost' = 'user_0d_2_pwd';
 CREATE DATABASE IF NOT EXISTS `hbtn_0d_2`;
-GRANT SELECT ON hbtn_0d_2 TO 'user_0d_1'@'localhost';
+GRANT USAGE ON *.* TO 'user_0d_2'@'localhost';
+GRANT SELECT ON `hbtn_0d_2`.* TO 'user_0d_2'@'localhost';
