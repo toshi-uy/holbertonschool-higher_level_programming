b88fde7ff7bd94208bee335b8e38474b1cee6213
toshi-uy <s_borgia@hotmail.com>
2021-07-13 19:19:56 -0300

    inner join

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -7,7 +7,7 @@
 
 SELECT tv_genres.name
 FROM tv_genres
-JOIN tv_show_genres ON tv_show_genres.genre_id = tv_genres.id
-JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+INNER JOIN tv_show_genres ON tv_show_genres.genre_id = tv_genres.id
+INNER JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
 WHERE tv_shows.title = 'Dexter'
 ORDER BY tv_genres.name DESC;
