e557b15d66150aebbadb4a5fcee62a94dd5ff5f4
toshi-uy <s_borgia@hotmail.com>
2021-07-13 16:53:02 -0300

    shows

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -4,7 +4,7 @@
 -- You can use only one SELECT statement
 -- The database name will be passed as an argument of the mysql command
 
-SELECT tv_show.title, tv_show_genres.genre_id
+SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
 JOIN tv_show_genres ON tv_show_genres.genre_id = tv_shows.title
 WHERE tv_shows.genre != NULL
