eba30be677b1ed43ee5f437cc94cd79f8a90b5a9
toshi-uy <s_borgia@hotmail.com>
2021-07-13 19:20:40 -0300

    asc

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -10,4 +10,4 @@ FROM tv_genres
 INNER JOIN tv_show_genres ON tv_show_genres.genre_id = tv_genres.id
 INNER JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
 WHERE tv_shows.title = 'Dexter'
-ORDER BY tv_genres.name DESC;
+ORDER BY tv_genres.name ASC;
