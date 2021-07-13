0e78e7711222653810010e95f248102fd0dff1e1
toshi-uy <s_borgia@hotmail.com>
2021-07-13 16:52:19 -0300

    testing multiple things

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,11 @@
+-- script that lists all shows contained in hbtn_0d_tvshows that have at least one genre linked.
+-- Each record should display: tv_shows.title - tv_show_genres.genre_id
+-- Results must be sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
+-- You can use only one SELECT statement
+-- The database name will be passed as an argument of the mysql command
+
+SELECT tv_show.title, tv_show_genres.genre_id
+FROM tv_shows
+JOIN tv_show_genres ON tv_show_genres.genre_id = tv_shows.title
+WHERE tv_shows.genre != NULL
+ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
