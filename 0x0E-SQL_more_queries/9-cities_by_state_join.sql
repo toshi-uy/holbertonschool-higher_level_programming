86c0ea793bd5b3116aac731227e0dafdcda6ad54
toshi-uy <s_borgia@hotmail.com>
2021-07-13 16:33:32 -0300

    FULL OUTER IS BETTER

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -4,6 +4,8 @@
 -- You can use only one SELECT statement
 -- The database name will be passed as an argument of the mysql command
 
-SELECT cities.id, cities.name FROM cities
-FULL OUTER JOIN states ON cities.names = states.names
+SELECT cities.id, cities.name
+FROM cities
+LEFT JOIN states
+ON cities.names = states.names
 ORDER BY cities.id ASC;
