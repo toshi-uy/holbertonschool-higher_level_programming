89898319a16e6a9f6fc8647cd749440848ecead5
toshi-uy <s_borgia@hotmail.com>
2021-07-13 16:57:49 -0300

    is not null

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -7,5 +7,5 @@
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
 JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-WHERE tv_show_genres.show_id != NULL
+WHERE tv_show_genres.show_id IS NOT NULL
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
