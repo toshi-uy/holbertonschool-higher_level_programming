400b215bfde9c464cdf67315d751281297c75ff7
toshi-uy <s_borgia@hotmail.com>
2021-07-13 15:45:12 -0300

    name

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -4,4 +4,4 @@
 -- You are not allowed to use the JOIN keyword
 -- The database name will be passed as an argument of the mysql command
 
-SELECT DISTINCT California FROM cities ORDER BY cities.id DESC;
+SELECT DISTINCT California FROM name ORDER BY cities.id DESC;
