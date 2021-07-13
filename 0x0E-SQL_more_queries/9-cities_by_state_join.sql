47916ab7253809593b42fbe7e32fd6abe7331a52
toshi-uy <s_borgia@hotmail.com>
2021-07-13 16:30:41 -0300

    testing INNER JOIN

---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,9 @@
+-- script that lists all cities contained in the database hbtn_0d_usa.
+-- Each record should display: cities.id - cities.name - states.name
+-- Results must be sorted in ascending order by cities.id
+-- You can use only one SELECT statement
+-- The database name will be passed as an argument of the mysql command
+
+SELECT cities.id, cities.name FROM cities
+A INNER JOIN states ON cities.names = states.names
+ORDER BY cities.id ASC;
