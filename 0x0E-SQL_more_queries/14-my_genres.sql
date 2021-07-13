855e4584cd0c4a58a3ce6bcc7be41df85c053d21
toshi-uy <s_borgia@hotmail.com>
2021-07-13 19:07:27 -0300

    name

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -10,4 +10,4 @@ FROM tv_genres
 JOIN tv_show_genres ON tv_show_genres.genre_id = tv_genres.id
 JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
 WHERE tv_genres.name = 'Dexter'
-ORDER BY number_of_shows DESC;
+ORDER BY tv_genres.name DESC;
