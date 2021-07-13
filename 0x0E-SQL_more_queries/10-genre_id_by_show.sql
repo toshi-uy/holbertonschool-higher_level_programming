63f9d2f909ff5199740cbc023d2f12d048ecd5d9
toshi-uy <s_borgia@hotmail.com>
2021-07-13 16:57:16 -0300

    ASC

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -8,4 +8,4 @@ SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
 JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 WHERE tv_show_genres.show_id != NULL
-ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
