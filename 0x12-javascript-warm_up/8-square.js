b22decd83ad2247bef1c9ad13b85fe23cebb6238
toshi-uy <s_borgia@hotmail.com>
2021-08-02 19:05:19 -0300

    testing print and printin module

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,4 +1,7 @@
 #!/usr/bin/node
+function print(s) { document.getElementById('out').innerHTML += s; }
+function println(s) { document.getElementById('out').innerHTML += s + '\n'; }
+
 let size = parseInt(process.argv[2]);
 let i, j;
 if (size === undefined || isNaN(size)) {
