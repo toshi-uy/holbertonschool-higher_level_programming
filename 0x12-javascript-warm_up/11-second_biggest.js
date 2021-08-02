3d398bc7267d69fab367d31d4bbea0d0ad83ae0d
toshi-uy <s_borgia@hotmail.com>
2021-08-02 20:28:47 -0300

    testing for

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+const argv = process.argv;
+
+if (argv.length === 2) {
+  console.log(0);
+} else if (argv.length === 3) {
+  console.log(0);
+} else {
+  let max = argv[0];
+  for (let i = 2; i < argv.length - 2; i++) {
+    if (argv[i] >= max) {
+      max = argv[i];
+    }
+  }
+  console.log(max);
+}
