ab1244faa99d4a533dd126caa49517031695c120
toshi-uy <s_borgia@hotmail.com>
2021-08-02 18:37:15 -0300

    logging not a number

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,3 +1,7 @@
 #!/usr/bin/node
 const number = parseInt(process.argv[2]);
-console.log('My number: ' + number);
+if (number === 'NaN') {
+  console.log('Not a number');
+} else {
+  console.log('My number: ' + number);
+}
