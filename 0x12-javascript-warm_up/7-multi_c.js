c8933b5257ca4965bc194ef5fbbbcd39e5c006e3
toshi-uy <s_borgia@hotmail.com>
2021-08-02 18:52:43 -0300

    let instead of const

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 const myVar = 'C is fun';
-const argv = process.argv[2];
+let argv = process.argv[2];
 if (argv === undefined) {
   console.log('Missing number of occurrences')
 }
 if(argv >= 0) {
   while (argv >= 0) {
     console.log(myVar);
-    argv -= 1;
+    argv--;
   }
 }
