3ceb78e0e76a8378a4204479b8f135683595b6b2
toshi-uy <s_borgia@hotmail.com>
2021-08-02 20:33:27 -0300

    debugging

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,15 +1,13 @@
 #!/usr/bin/node
 const argv = process.argv;
-console.log(argv);
-console.log(argv.length);
 if (argv.length === 2) {
   console.log(0);
 } else if (argv.length === 3) {
   console.log(0);
 } else {
   let max = argv[0];
-  for (let i = 2; i < argv.length - 1; i++) {
-    if (argv[i] >= max) {
+  for (let i = 2; i < argv.length; i++) {
+    if (argv[i] > max) {
       max = argv[i];
     }
   }
