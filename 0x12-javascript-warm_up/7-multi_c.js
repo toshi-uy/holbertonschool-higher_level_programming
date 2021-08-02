801604ea1f583e44a8eb3fc7fdaa97313c6f693c
toshi-uy <s_borgia@hotmail.com>
2021-08-02 18:56:32 -0300

    added semicolon

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -2,7 +2,7 @@
 const myVar = 'C is fun';
 let argv = process.argv[2];
 if (argv === undefined) {
-  console.log('Missing number of occurrences')
+  console.log('Missing number of occurrences');
 }
 if (argv > 0) {
   while (argv > 0) {
