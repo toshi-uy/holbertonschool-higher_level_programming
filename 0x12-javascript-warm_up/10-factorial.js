bd6953cd21438239fb9a51177465ce447e614e16
toshi-uy <s_borgia@hotmail.com>
2021-08-02 20:16:55 -0300

    added ()

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,12 +1,13 @@
 #!/usr/bin/node
 const num = process.argv[2];
 function factorial(x) {
-  if (x === 0) {
-      return 1;
+  if (x < 0) {
+    console.log("num must not be negative");
   }
-  else {
-      return (x * factorial(x - 1));
+  if (x <= 1) {
+    return 1;
   }
+  return x * factorial(x - 1);
 }
 
 let result = factorial(3);
