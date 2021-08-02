14843f0cace84bb5e3012ec973591135cc1744b9
toshi-uy <s_borgia@hotmail.com>
2021-08-02 20:49:25 -0300

    testing backwards

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -7,8 +7,8 @@ if (argv.length === 2) {
 } else {
   let secondMax = argv[2];
   argv.sort();
-  for (let i = 2; i < argv.length - 1; i++) {
-    if (argv[i] < argv[i + 1]) {
+  for (let i = 3; i < argv.length; i++) {
+    if (argv[i] > argv[i - 1]) {
       secondMax = argv[i];
     }
   }
