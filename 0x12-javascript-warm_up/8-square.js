0f75d438b283ea561c3f8e62216d984d41436b2b
toshi-uy <s_borgia@hotmail.com>
2021-08-02 19:14:05 -0300

    added standard fixes

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,13 +1,13 @@
 #!/usr/bin/node
-let size = parseInt(process.argv[2]);
+const size = parseInt(process.argv[2]);
 if (size === undefined || isNaN(size)) {
   console.log('Missing size');
 }
 if (size > 0) {
   for (let i = 0; i < size; i++) {
     for (let j = 0; j < size; j++) {
-      process.stdout.write("X");
+      process.stdout.write('X');
     }
-    console.log("");
+    console.log('');
   }
 }
