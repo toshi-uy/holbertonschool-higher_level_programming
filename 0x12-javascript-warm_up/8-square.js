9a417f0b2a380cb8cf39dc63c20e318cd7d09d63
toshi-uy <s_borgia@hotmail.com>
2021-08-02 19:08:03 -0300

    changed to console.log

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,17 +1,13 @@
 #!/usr/bin/node
-function print(s) { document.getElementById('out').innerHTML += s; }
-function println(s) { document.getElementById('out').innerHTML += s + '\n'; }
-
 let size = parseInt(process.argv[2]);
-let i, j;
 if (size === undefined || isNaN(size)) {
   console.log('Missing size');
 }
 if (size > 0) {
-  for (i = 0; i < size; i++) {
-    for (j = 0; j < size; j++) {
-      print('X');
+  for (let i = 0; i < size; i++) {
+    for (let j = 0; j < size; j++) {
+      console.log("X");
     }
-    println('');
+    console.log("");
   }
 }
