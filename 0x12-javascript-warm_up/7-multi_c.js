ab2a0e7e4924ca7cce370cc2eeb0322d5fb25831
toshi-uy <s_borgia@hotmail.com>
2021-08-02 18:50:12 -0300

    testing null condition

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const myVar = 'C is fun';
+const argv = process.argv[2];
+if (argv === null) {
+  console.log('Missing number of occurrences')
+}
+if(argv >= 0) {
+  while (argv >= 0) {
+    console.log(myVar);
+    argv--;
+  }
+}
