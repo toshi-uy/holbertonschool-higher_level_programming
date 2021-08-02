9f5e9e01aa7b4d2415c749c459e51ddc2918c154
toshi-uy <s_borgia@hotmail.com>
2021-08-02 16:00:25 -0300

    fixed standard

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -3,8 +3,7 @@ const argv = process.argv.length;
 
 if (argv === 2) {
   console.log('No argument');
-}
-else if (argv === 3) {
+} else if (argv === 3) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
