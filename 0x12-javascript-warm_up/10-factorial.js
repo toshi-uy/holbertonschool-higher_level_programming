839cb10d85071fe12f8f55a7f4bd09ed9824eaa3
toshi-uy <s_borgia@hotmail.com>
2021-08-02 19:31:38 -0300

    testing recursive

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const num = parseInt(process.argv[2]);
+function factorial(x) {
+  if (x === 0) {
+      return 1;
+  }
+  else {
+      return x * factorial(x - 1);
+  }
+}
+if (num > 0) {
+  console.log(factorial(num));
+}
