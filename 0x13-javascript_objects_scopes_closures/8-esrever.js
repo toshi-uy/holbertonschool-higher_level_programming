030c757bccc772c653c8d8d18e1d55361113c67b
toshi-uy <s_borgia@hotmail.com>
2021-08-03 20:59:47 -0300

    testing for

---
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+exports.esrever = function (list) {
+  let j = list.length - 1;
+  for (let i = 0, j; i <= j; i++, j--) {
+    const temp = list[i];
+    list[i] = list[j];
+    list[j] = temp;
+  }
+  return (list);
+};
