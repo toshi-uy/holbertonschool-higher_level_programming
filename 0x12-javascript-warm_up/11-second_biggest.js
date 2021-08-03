1fc8d8d5685b2249d7c82c32178199263924591c
toshi-uy <s_borgia@hotmail.com>
2021-08-02 21:06:23 -0300

    sort

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -5,12 +5,6 @@ if (argv.length === 2) {
 } else if (argv.length === 3) {
   console.log(0);
 } else {
-  let secondMax = argv[2];
-  argv.sort();
-  for (let i = 2; i < argv.length - 1; i++) {
-    if (argv[i] > argv[i + 1]) {
-      secondMax = argv[i];
-    }
-  }
-  console.log(secondMax);
+  process.argv.sort((a, b) => a - b);
+  console.log(process.argv[process.argv.length - 2]);
 }
