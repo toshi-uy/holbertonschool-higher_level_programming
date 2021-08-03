df967dfc7d96ea07a746758544b97380243631b6
toshi-uy <s_borgia@hotmail.com>
2021-08-03 19:52:38 +0000

    added fixes to semistandard

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 
 exports.nbOccurences = function (list, searchElement) {
-    let ocurrency = 0;
-    list.forEach(element => {
-        if (element === searchElement) {
-            ocurrency += 1;
-        }
-    });
-    return ocurrency;
-}
+  let ocurrency = 0;
+  list.forEach(element => {
+    if (element === searchElement) {
+      ocurrency += 1;
+    }
+  });
+  return ocurrency;
+};
