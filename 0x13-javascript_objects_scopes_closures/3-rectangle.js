c61e83163174b2bd64872302a9a20ee5e99a7199
toshi-uy <s_borgia@hotmail.com>
2021-08-03 16:08:41 -0300

    print outside of constructor

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -6,13 +6,13 @@ class Rectangle {
       this.width = w;
       this.height = h;
     }
-    print () {
-      for (let i = 0; i < this.height; i++) {
-        for (let j = 0; j < this.width; j++) {
-          process.stdout.write('X');
-        }
-        console.log('');
+  }
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      for (let j = 0; j < this.width; j++) {
+        process.stdout.write('X');
       }
+      console.log('');
     }
   }
 }
