4792a934ea54750110c5b0f18600078d88ebe0cd
toshi-uy <s_borgia@hotmail.com>
2021-08-02 21:04:20 -0300

    <

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -5,10 +5,10 @@ if (argv.length === 2) {
 } else if (argv.length === 3) {
   console.log(0);
 } else {
-  let secondMax = argv[3];
+  let secondMax = argv[2];
   argv.sort();
-  for (let i = 3; i < argv.length; i++) {
-    if (argv[i] < argv[i - 1]) {
+  for (let i = 2; i < argv.length - 1; i++) {
+    if (argv[i] < argv[i + 1]) {
       secondMax = argv[i];
     }
   }
