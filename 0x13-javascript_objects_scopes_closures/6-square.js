26e13a4424f3d2fe26699825ff2c3bda3762d17e
toshi-uy <s_borgia@hotmail.com>
2021-08-03 16:28:36 -0300

    testng print

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+const Square = require('./5-square');
+
+class Square extends Square {
+  constructor (size) {
+    super(size);
+  }
+  charPrint(c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    for (let i = 0; i < this.size; i++) {
+      for (let j = 0; j < this.size; j++) {
+        process.stdout.write(c);
+      }
+      console.log('');
+    }
+  }
+}
+module.exports = Square;
