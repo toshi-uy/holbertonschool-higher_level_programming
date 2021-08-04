79387559a631fb4a152eb3240f520562c9450536
toshi-uy <s_borgia@hotmail.com>
2021-08-03 21:38:19 -0300

    was 100-data

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-const list = require('./100-map.js').list;
+const list = require('./100-data.js').list;
 console.log(list);
 const newList = list.map((x, index) => x * index);
 console.log(newList);
