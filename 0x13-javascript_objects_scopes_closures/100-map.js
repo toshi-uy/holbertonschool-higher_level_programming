6f399249ea7f7ad119f69fce918ef88f3c2202f0
toshi-uy <s_borgia@hotmail.com>
2021-08-03 21:32:19 -0300

    added {}

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -2,5 +2,5 @@
 
 const list = require('./100-map.js');
 console.log(list);
-let newList = list.map((x, index) => x * index);
+let newList = list.map((x, index) => {x * index});
 console.log(newList);
