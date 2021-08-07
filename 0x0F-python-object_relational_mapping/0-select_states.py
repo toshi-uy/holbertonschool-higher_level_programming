50c9e17c3f70012540c1237fdae7759a110c4150
toshi-uy <s_borgia@hotmail.com>
2021-08-06 22:10:59 -0300

    added port

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -8,8 +8,7 @@ if __name__ == "__main__":
     MY_U = argv[0]
     MY_PS = argv[1]
     MY_DB = argv[2]
-    connect = MySQLdb.connect(host=MY_H, user=MY_U,
-                              passwd=MY_PS, db=MY_DB, port=3306)
+    connect = MySQLdb.connect(host=MY_H, user=MY_U, passwd=MY_PS, db=MY_DB, port=3306)
     cursor = connect.cursor()
     cursor.execute("SELECT * FROM states ORDER BY id")
     querry = cursor.fetchall()
