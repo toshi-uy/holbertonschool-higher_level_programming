d8b03cbc2cf54917b3e78c684a8f5e23ffd9d335
toshi-uy <s_borgia@hotmail.com>
2021-08-06 22:15:54 -0300

    added () in if

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 """script that lists all states from the database hbtn_0e_0_usa"""
 
-if __name__ == "__main__":
+if (__name__ == "__main__"):
     import MySQLdb
     from sys import argv
-    MY_H = "localhost"
-    MY_U = argv[1]
-    MY_PS = argv[2]
-    MY_DB = argv[3]
-    connect = MySQLdb.Connect(host=MY_H, user=MY_U, passwd=MY_PS, db=MY_DB, port=3306)
+    HOST = "localhost"
+    USER = argv[1]
+    PASSWRD = argv[2]
+    DATABASE = argv[3]
+    connect = MySQLdb.Connect(host=HOST, user=USER, passwd=PASSWRD, db=DATABASE, port=3306)
     cursor = connect.cursor()
     cursor.execute("SELECT * FROM states ORDER BY id")
     querry = cursor.fetchall()
