e14ca44792e2fc7ae898a421ebb4706368ba3528
toshi-uy <s_borgia@hotmail.com>
2021-08-07 20:26:02 +0000

    added file and fixed pepocho

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""script that lists all states from the database hbtn_0e_0_usa"""
+
+if (__name__ == "__main__"):
+    import MySQLdb
+    from sys import argv
+
+    USER = argv[1]
+    PASSWRD = argv[2]
+    DATABASE = argv[3]
+    connect = MySQLdb.Connect(host="localhost", user=USER,
+                              passwd=PASSWRD, db=DATABASE, port=3306,
+                              charset="utf8")
+    cursor = connect.cursor()
+    text = "SELECT id, name FROM states WHERE name LIKE 'N%' ORDER BY id"
+    cursor.execute(text)
+    querry = cursor.fetchall()
+    for line in querry:
+        print(line)
+    cursor.close()
+    connect.close()
