a3167eb80e447ef56106a823c80dbbabffc63b6a
toshi-uy <s_borgia@hotmail.com>
2021-08-08 12:08:07 -0300

    added if and flag

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+script that lists all State objects from the database hbtn_0e_6_usa
+"""
+
+if (__name__ == "__main__"):
+    from sys import argv
+    from model_state import Base, State
+    from sqlalchemy import (create_engine)
+    from sqlalchemy.orm import sessionmaker
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(argv[1], argv[2], argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    flag = 0
+    for instance in session.query(State).order_by(State.id):
+        if argv[4] == instance.name:
+            print("{}: {}".format(instance.id, instance.name))
+            flag = 1
+    if flag == 0:
+        print("Not found")
+    session.close()
