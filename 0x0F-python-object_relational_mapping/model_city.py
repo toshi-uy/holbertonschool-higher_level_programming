b4ae9a4b765ec3066319b4ab72f035b9b5175d0c
toshi-uy <s_borgia@hotmail.com>
2021-08-08 15:24:40 -0300

    import base

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -12,6 +12,7 @@ You must use the module SQLAlchemy
 
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
+from model_state import Base
 
 
 class City(Base):
