278f44a4ea3c8da8de7a0c58db28ab45fdb67fdf
toshi-uy <s_borgia@hotmail.com>
2021-08-08 12:04:28 -0300

    added if

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+script that lists all State objects from the database hbtn_0e_6_usa
+"""
+
+if (__name__ == "__main__"):
+    from sys import argv
+    from model_state import Base, State
+    from sqlalchemy import (create_engine)
+    from sqlalchemy.orm import sessionmaker
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(argv[1], argv[2], argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    for instance in session.query(State).order_by(State.id):
+        if 'a' in instance.name or 'A' in instance.name:
+            print("{}: {}".format(instance.id, instance.name))
+    session.close()
