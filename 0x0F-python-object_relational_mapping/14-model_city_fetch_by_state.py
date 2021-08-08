959622e81b8d2b05355a8ce030cdc11da6697502
toshi-uy <s_borgia@hotmail.com>
2021-08-08 15:30:01 -0300

    fixing long statements for pep8

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -19,6 +19,6 @@ if (__name__ == "__main__"):
     Session = sessionmaker(bind=engine)
     session = Session()
     for state, city in session.query(State, City).\
-        filter(State.id == City.state_id):
+filter(State.id == City.state_id):
         print("{}: ({}) {}".format(state.name, city.id, city.name))
     session.close()
