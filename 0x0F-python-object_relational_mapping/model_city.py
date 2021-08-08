b34f41e11f9cb1b3c3ad0a970b843fbf248e0191
toshi-uy <s_borgia@hotmail.com>
2021-08-08 15:24:01 -0300

    changed foreing before null

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -19,4 +19,4 @@ class City(Base):
     __tablename__ = 'cities'
     id = Column(Integer, unique=True, autoincrement=True, nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, nullable=False, ForeignKey("states.id"))
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
