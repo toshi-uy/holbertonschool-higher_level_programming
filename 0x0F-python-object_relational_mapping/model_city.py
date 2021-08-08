d445b040bd5b7507ede0726e8ea4726c2fa54e39
toshi-uy <s_borgia@hotmail.com>
2021-08-08 15:26:17 -0300

    fixing long statements for pep8

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -4,9 +4,12 @@ python file that contains the class definition of a City
 City class:
 inherits from Base (imported from model_state)
 links to the MySQL table cities
-class attribute id that represents a column of an auto-generated, unique integer, can’t be null and is a primary key
-class attribute name that represents a column of a string of 128 characters and can’t be null
-class attribute state_id that represents a column of an integer, can’t be null and is a foreign key to states.id
+class attribute id that represents a column of an 
+auto-generated, unique integer, can’t be null and is a primary key
+class attribute name that represents a column of a string of
+128 characters and can’t be null
+class attribute state_id that represents a column of an integer,
+can’t be null and is a foreign key to states.id
 You must use the module SQLAlchemy
 """
 
@@ -18,6 +21,7 @@ from model_state import Base
 class City(Base):
     """class definition of a City"""
     __tablename__ = 'cities'
-    id = Column(Integer, unique=True, autoincrement=True, nullable=False, primary_key=True)
+    id = Column(Integer, unique=True, autoincrement=True, 
+                nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
