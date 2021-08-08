284836767b0aecaa6ab9eaa2d1713335ddb8e361
toshi-uy <s_borgia@hotmail.com>
2021-08-08 14:22:05 -0300

    added filter and update

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-script that lists all State objects from the database hbtn_0e_6_usa
+script that changes the name of a State object from the database hbtn_0e_6_usa
 """
 
 if (__name__ == "__main__"):
