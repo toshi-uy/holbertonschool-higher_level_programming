6629cb8f2e5d18415e9e2bb1ebaa8e9f4694ea3c
toshi-uy <s_borgia@hotmail.com>
2021-08-08 15:26:42 -0300

    changed foreing before null

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -4,7 +4,7 @@ python file that contains the class definition of a City
 City class:
 inherits from Base (imported from model_state)
 links to the MySQL table cities
-class attribute id that represents a column of an 
+class attribute id that represents a column of an
 auto-generated, unique integer, can’t be null and is a primary key
 class attribute name that represents a column of a string of
 128 characters and can’t be null
@@ -21,7 +21,7 @@ from model_state import Base
 class City(Base):
     """class definition of a City"""
     __tablename__ = 'cities'
-    id = Column(Integer, unique=True, autoincrement=True, 
+    id = Column(Integer, unique=True, autoincrement=True,
                 nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
