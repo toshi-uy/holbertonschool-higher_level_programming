debe148ab345fe821bc350b37daf3213c0721254
toshi-uy <s_borgia@hotmail.com>
2021-08-08 11:42:00 -0300

    fixing misindent

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -9,9 +9,9 @@ if (__name__ == "__main__"):
     from sqlalchemy import (create_engine)
     from sqlalchemy.orm import sessionmaker
 
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'\
-                           .format(argv[1], argv[2], argv[3]),\
-                            pool_pre_ping=True)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(argv[1], argv[2], argv[3]),
+                           pool_pre_ping=True)
     Base.metadata.create_all(engine)
 
     Session = sessionmaker(bind=engine)
