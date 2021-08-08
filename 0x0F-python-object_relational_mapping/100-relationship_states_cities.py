405446de61309e85803c2686f6244df25d361943
toshi-uy <s_borgia@hotmail.com>
2021-08-08 20:53:45 -0300

    added correct import

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -6,10 +6,10 @@ the letter a from the database hbtn_0e_6_usa
 
 if (__name__ == "__main__"):
     from sys import argv
-    from model_state import Base, State
+    from relationship_state import Base, State
+    from relationship_city import City
     from sqlalchemy import (create_engine)
     from sqlalchemy.orm import sessionmaker
-    from model_city import City
 
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
                            .format(argv[1], argv[2], argv[3]),
