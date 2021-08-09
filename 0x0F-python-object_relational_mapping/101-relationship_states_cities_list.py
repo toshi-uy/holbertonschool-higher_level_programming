ae978dc497bb9dee597bb614ac8ce1bb31208857
toshi-uy <s_borgia@hotmail.com>
2021-08-08 22:50:04 -0300

    testing relation

---
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+script that deletes all State objects with a name containing
+the letter a from the database hbtn_0e_6_usa
+"""
+
+if (__name__ == "__main__"):
+    from sys import argv
+    from relationship_state import Base, State
+    from relationship_city import City
+    from sqlalchemy import (create_engine)
+    from sqlalchemy.orm import sessionmaker
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(argv[1], argv[2], argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    for state, city in session.query(State, state.cities).order_by(State.id):
+        print("{}: {}\n\t{}: {}".format(state.id, state.name, city.id, city.name))
+    session.close()
