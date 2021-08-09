ba7a9d5f68d8c2cbe05c4378051b3579b540e688
toshi-uy <s_borgia@hotmail.com>
2021-08-08 21:06:30 -0300

    added realtionship in city

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -15,7 +15,7 @@ You must use the module SQLAlchemy
 
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
-from model_state import Base
+from relationship_state import Base, State
 
 
 class City(Base):
