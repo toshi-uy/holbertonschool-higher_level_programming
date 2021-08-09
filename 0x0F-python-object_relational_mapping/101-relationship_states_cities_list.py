f57d84827e86e843c78ea194c630731ab4b1c291
toshi-uy <s_borgia@hotmail.com>
2021-08-08 22:56:01 -0300

    state.cities in second for

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -20,6 +20,6 @@ if (__name__ == "__main__"):
     session = Session()
     for state in session.query(State).order_by(State.id):
         print("{}: {}".format(state.id, state.name))
-        for city in state.city:
+        for city in state.cities:
             print("\t{}: {}".format(city.id, city.name))
     session.close()
