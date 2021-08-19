0448bf51739b823d606fba13ee4b811c14a2a9f9
toshi-uy <s_borgia@hotmail.com>
2021-08-19 14:19:21 -0300

    removed :

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -2,4 +2,4 @@
 # Bash script that takes in a URL, sends a request to that URL
 # and displays the size of the body of the response
 
-curl -Is  0.0.0.0:5000 | grep 'Content-Length: ' | awk '{print $2}'
+curl -Is  0.0.0.0:5000 | grep 'Content-Length' | awk '{print $2}'
