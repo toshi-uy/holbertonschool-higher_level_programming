893bb70dbf880aa40cdf6c90fa88298d40d66f46
toshi-uy <s_borgia@hotmail.com>
2021-08-19 15:17:27 -0300

    addded comments

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Bash script that takes in a URL, sends a request to that URL
+# Bash script that takes in a URL and displays all HTTP methods the server will accept
 curl -Is "$1" | grep 'Allow' | cut -d " " -f 2-
