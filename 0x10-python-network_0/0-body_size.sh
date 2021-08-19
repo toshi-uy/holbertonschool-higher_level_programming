391a97d3fba2e0b8ca962dc7e846dc62113ad132
toshi-uy <s_borgia@hotmail.com>
2021-08-19 14:48:32 -0300

    removed one line

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 # Bash script that takes in a URL, sends a request to that URL
-# and displays the size of the body of the response
 
 curl -Is "$1" | grep 'Content-Length' | awk '{print $2}'
