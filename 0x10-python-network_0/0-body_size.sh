d37b92b50675e79b968f64bdb8021f23dbb7cb90
toshi-uy <s_borgia@hotmail.com>
2021-08-19 14:47:22 -0300

    added ""

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -2,4 +2,4 @@
 # Bash script that takes in a URL, sends a request to that URL
 # and displays the size of the body of the response
 
-curl -Is $1 | grep 'Content-Length' | awk '{print $2}'
+curl -Is "$1" | grep 'Content-Length' | awk '{print $2}'
