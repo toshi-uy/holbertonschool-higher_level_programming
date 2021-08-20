ad0c8cb3102bdcf34e206fb9e01e6833920eb1cd
toshi-uy <s_borgia@hotmail.com>
2021-08-19 22:44:34 -0300

    too many lines

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -18,6 +18,6 @@ def find_peak(list_of_integers):
         else:
             for i in range(1, (len(list_of_integers) - 1)):
                 if list_of_integers[i] >= list_of_integers[i - 1]\
-                    and list_of_integers[i] >= list_of_integers[i + 1]:
+                   and list_of_integers[i] >= list_of_integers[i + 1]:
                     peak = list_of_integers[i]
         return peak
