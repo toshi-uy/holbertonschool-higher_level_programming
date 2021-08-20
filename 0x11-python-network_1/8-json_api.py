ed2b2fbbf28b5df510ca0a9d8ca14b211f1ef05c
toshi-uy <s_borgia@hotmail.com>
2021-08-20 20:57:17 -0300

    testing try, except

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+Python script that takes in a letter and sends a POST request to
+http://0.0.0.0:5000/search_user with the letter as a parameter.
+    -The letter must be sent in the variable q
+    -If no argument is given, set q=""
+    -If the response body is properly JSON formatted and not empty,
+    display the id and name like this: [<id>] <name>
+    Otherwise:
+        -Display Not a valid JSON if the JSON is invalid
+        -Display No result if the JSON is empty
+"""
+
+if __name__ == "__main__":
+    import requests
+    import sys
+
+    if len(sys.argv) == 1:
+        data = {'q' : ""}
+    else:
+        data = {'q' : sys.argv[2]}
+    req = requests.post(sys.argv[1], data)
+    try:
+        if(len(req.json) == 0):
+            print("No result")
+        else:
+            jeison = req.json()
+            print("[{}] {}".format(jeison.get('id'),jeison.get('name')))git pull
+    except:
+        print("Not a valid JSON")
