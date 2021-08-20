47127a718bbd95e451c39870c4ef8cdaa2dd2602
toshi-uy <s_borgia@hotmail.com>
2021-08-20 11:50:57 -0300

    added print

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -8,4 +8,5 @@ if __name__ == "__main__":
     with urllib.request.urlopen(req) as response:
         html = response.read()
         print("Body response:")
+        print("\t- type:",type(html))
         print("\t- content:",html)
