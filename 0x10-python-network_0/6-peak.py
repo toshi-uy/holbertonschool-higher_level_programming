5d7e5a20fafd649c6ca886e814c58f96e880a9ed
toshi-uy <s_borgia@hotmail.com>
2021-08-19 22:41:37 -0300

    return None

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -9,13 +9,14 @@ def find_peak(list_of_integers):
     
     if list_of_integers == [None]:
         return None
-    peak = 0
-    if list_of_integers[0] >= list_of_integers[1]:
-        peak = list_of_integers[0]
-    elif list_of_integers[-1] >= list_of_integers[-2]:
-        peak = list_of_integers[-1]
     else:
-        for i in range(1, (len(list_of_integers) - 1)):
-            if list_of_integers[i] >= list_of_integers[i - 1] and list_of_integers[i] >= list_of_integers[i + 1]:
-                peak = list_of_integers[i]
-    return peak
+        peak = 0
+        if list_of_integers[0] >= list_of_integers[1]:
+            peak = list_of_integers[0]
+        elif list_of_integers[-1] >= list_of_integers[-2]:
+            peak = list_of_integers[-1]
+        else:
+            for i in range(1, (len(list_of_integers) - 1)):
+                if list_of_integers[i] >= list_of_integers[i - 1] and list_of_integers[i] >= list_of_integers[i + 1]:
+                    peak = list_of_integers[i]
+        return peak
