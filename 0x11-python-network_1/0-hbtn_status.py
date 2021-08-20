6ff63024b625127a54129f4a4629bce30bb2ac06
toshi-uy <s_borgia@hotmail.com>
2021-08-20 11:46:35 -0300

    added print

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -6,4 +6,5 @@ if __name__ == "__main__":
 
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         html = response.read()
+        print(response)
         print(html)
