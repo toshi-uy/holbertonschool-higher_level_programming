5c5da44ff61dcea35479861afd2a303291cae09d
toshi-uy <s_borgia@hotmail.com>
2021-08-20 19:00:10 -0300

    added file 4

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Python script that takes in a URL and an email,
+sends a POST request to the passed URL with the email
+as a parameter, and displays the body of the response (decoded in utf-8)
+"""
+
+if __name__ == "__main__":
+    import requests
+
+    respond = requests.get('https://intranet.hbtn.io/status')
+    content = respond.content
+    print("Body response:")
+    print("\t- type:", content['type'])
+    print("\t- content:", content['content'])
