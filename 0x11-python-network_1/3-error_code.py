cc9c710d52732b7255eaee53b73425d439a4f8c3
toshi-uy <s_borgia@hotmail.com>
2021-08-20 15:08:23 -0300

    testing task 3

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Python script that takes in a URL and an email,
+sends a POST request to the passed URL with the email
+as a parameter, and displays the body of the response (decoded in utf-8)
+"""
+
+if __name__ == "__main__":
+    import urllib.request, urllib.error
+    import sys
+
+    if sys.argv[1]:
+        req = urllib.request.Request(sys.argv[1])
+        with urllib.request.urlopen(req) as response:
+            try:
+                info = response.info()
+                print(info['X-Request-Id'])
+            except:
+                print (req.getcode())
