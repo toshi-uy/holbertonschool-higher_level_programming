bf28ac5c35da019b20a1dd54b957771f263ba53f
toshi-uy <s_borgia@hotmail.com>
2021-08-20 13:31:46 -0300

    sys.argv

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -6,9 +6,9 @@ variable found in the header of the response.
 
 if __name__ == "__main__":
     import urllib.request
-    from sys import argv
+    import sys
 
-    req = urllib.request.Request(argv[1])
+    req = urllib.request.Request(sys.argv[1])
     with urllib.request.urlopen(req) as response:
         info = response.info()
         print(info['X-Request-Id'])
