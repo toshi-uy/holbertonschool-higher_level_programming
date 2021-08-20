7ed6a8c209a8d460ceb8211176322b4a7bc32c70
toshi-uy <s_borgia@hotmail.com>
2021-08-20 20:38:45 -0300

    addede file

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Python script that takes in a URL, sends a request to the URL
+and displays the body of the response
+"""
+
+if __name__ == "__main__":
+    import requests
+    import sys
+
+    if sys.argv[1]:
+        req = requests.get(sys.argv[1])
+        print("Error code:", req.status_code)
+
