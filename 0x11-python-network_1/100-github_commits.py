8cc0804b1553f5111b4143cd8fdffe5ec8d2f818
toshi-uy <s_borgia@hotmail.com>
2021-08-20 22:14:45 -0300

    removed prints

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -14,10 +14,8 @@ if __name__ == "__main__":
         respond = requests.get('https://api.github.com/repos/{}/{}/commits'
                      .format(sys.argv[2], sys.argv[1]))
         commits = respond.json()
-        print(commits)
         i = 0
         for commit in commits:
-            print(commit)
             if i <= 9:
                 print("{}: {}".format(
                     commit.get('sha'),
