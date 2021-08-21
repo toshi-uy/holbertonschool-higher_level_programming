08de6ba2cef090d4bbba5c78b1f4b206e4c912a5
toshi-uy <s_borgia@hotmail.com>
2021-08-20 22:04:06 -0300

    testing las get

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+list 10 commits (from the most recent to oldest) of the repository
+“rails” by the user “rails”
+You must use the GitHub API, Print all commits by: `<sha>: <author name>`
+(one by line)
+"""
+
+if __name__ == "__main__":
+    import requests
+    import sys
+
+    if len(sys.argv) == 3:
+        url = 'https://api.github.com/repos/' + sys.argv[1] + '/' + sys.argv[2] + '/comits'
+        respond = requests.get(url)
+        commits = respond.json()
+        for commit in commits[:10]:
+            print("{}: {}".format(commit.get('sha'), commit.get('commit').get('author').get('name')))
