d4a6be675c434138ddf598f3db8e39c01dd2a3bf
toshi-uy <s_borgia@hotmail.com>
2021-09-14 14:24:35 -0300

    changed spaces

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,7 +3,7 @@ const argv = process.argv[2];
 const fs = require('fs');
 try {
   const data = fs.readFileSync(argv, 'utf8');
-  console.log(data.toString());    
+  console.log(data.toString());
 } catch (e) {
   console.log('Error:', e);
 }
