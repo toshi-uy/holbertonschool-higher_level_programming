f082875c52093eeb300877052e34268e3fdc8ffc
toshi-uy <s_borgia@hotmail.com>
2021-09-14 20:41:45 -0300

    testing 101

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -8,19 +8,15 @@ request(url, options, (error, res, body) => {
     return console.log(error);
   }
   if (!error && res.statusCode === 200) {
-    const unsorted = []
     body.characters.forEach(element => {
-      unsorted.append(element);
-      const sorted = unsorted.sort()
-      for (let i = 0; i < length(sorted); i++) {
-        request(element, options, (error, res, body) => {
-          if (error) {
-            return console.log(error);
-          }
-          if (!error && res.statusCode === 200) {
-            console.log(body.name);
-          }
+      request(element, options, (error, res, body) => {
+        if (error) {
+          return console.log(error);
+        }
+        if (!error && res.statusCode === 200) {
+          console.log(body.name);
+        }
       });
-    }
+    });
   }
 });
--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -8,7 +8,12 @@ request(url, options, (error, res, body) => {
     return console.log(error);
   }
   if (!error && res.statusCode === 200) {
-    sorted(body.characters).forEach(element => {
+    const unsorted = []
+    body.characters.forEach(element => {
+      unsorted.append(element);
+    })
+    const sorted = unsorted.sort()
+    for (let i = 0; i < length(sorted); i++) {
       request(element, options, (error, res, body) => {
         if (error) {
           return console.log(error);
@@ -16,7 +21,7 @@ request(url, options, (error, res, body) => {
         if (!error && res.statusCode === 200) {
           console.log(body.name);
         }
-      });
-    });
+      })
+    }
   }
 });
