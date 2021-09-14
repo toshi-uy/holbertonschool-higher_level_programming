77868c933fcba2ff3cc9e452660ceaeeb1dc2785
toshi-uy <s_borgia@hotmail.com>
2021-09-14 20:00:23 -0300

    removing count variable

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -2,19 +2,19 @@
 const url = process.argv[2];
 const request = require('request');
 const options = { json: true };
-let results = {}
+const results = {};
 request(url, options, (error, res, body) => {
   if (error) {
     return console.log(error);
   }
   if (!error && res.statusCode === 200) {
     body.forEach(tasks => {
-        if (tasks.completed)
-          if (!results[tasks.userId])
-            results[tasks.userId] = 1;
-          else
-            results[tasks.userId] += 1;
+      if (tasks.completed)
+        if (!results[tasks.userId])
+          results[tasks.userId] = 1;
+        else
+          results[tasks.userId] += 1;
     });
-    console.log(results)
+    console.log(results);
   }
 });
