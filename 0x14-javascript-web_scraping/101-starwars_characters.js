70c0dcf1914967853eaa739153d32dd611e7b998
toshi-uy <s_borgia@hotmail.com>
2021-09-14 20:47:31 -0300

    testing 101

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -9,6 +9,7 @@ request(url, options, (error, res, body) => {
   }
   if (!error && res.statusCode === 200) {
     let unsorted = body.characters;
+    console.log(unsorted);
     const sorted = unsorted.sort()
     for (let i = 0; i < sorted.length; i++) {
       request(sorted[i], options, (error, res, body) => {
