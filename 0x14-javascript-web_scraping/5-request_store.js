3fe7e7851bd66501c1662d28cb89a8ca043ca917
toshi-uy <s_borgia@hotmail.com>
2021-09-14 16:31:57 -0300

    testing

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -2,12 +2,11 @@
 const argv1 = process.argv[2];
 const argv2 = process.argv[3];
 const req = require('request');
-try {
-  const data = request.get(argv1, 'utf8');
-  const fs = require('fs');
-  fs.appendFile(data, argv2, function (err) {
+const fs = require('fs');
+
+request.get(argv1, function (err, response) {
+  if (err) throw err;
+  fs.appendFile(response.body, argv2, function (err) {
     if (err) throw err;
   });
-} catch (e) {
-  console.log('Error:', e);
-}
+});
