57bcff3908328e0c8bee0022ab7461b0824bb375
toshi-uy <s_borgia@hotmail.com>
2021-09-14 15:37:02 -0300

    testing data

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -2,7 +2,6 @@
 const filmNbr = process.argv[2];
 const request = require('request');
 const url = 'https://swapi-api.hbtn.io/api/films/' + filmNbr
-request.get(url, function (err, body) {
-  if (err) throw err;
-  console.log(body.title);
-});
+let rawdata = request.readFileSync(url);
+let student = JSON.parse(rawdata);
+console.log(student.title);
