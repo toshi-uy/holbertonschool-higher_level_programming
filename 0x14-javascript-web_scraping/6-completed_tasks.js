a69fc15f39dae115fec941843af41c2c7e6d4afd
toshi-uy <s_borgia@hotmail.com>
2021-09-14 17:11:48 -0300

    what's inside elements?

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -2,13 +2,21 @@
 const url = process.argv[2];
 const request = require('request');
 const options = { json: true };
+let dict = {}
 request(url, options, (error, res, body) => {
   if (error) {
     return console.log(error);
   }
   if (!error && res.statusCode === 200) {
-    body.forEach(element => { 
-      console.log(element.userId);
+    body.forEach(tasks => {
+      tasks.forEach(element => {
+        if (tasks.completed === true)
+          dict.push({
+            user = tasks.userId,
+            completed = true
+          })
+      });
     });
+    console.log(dict)
   }
 });
