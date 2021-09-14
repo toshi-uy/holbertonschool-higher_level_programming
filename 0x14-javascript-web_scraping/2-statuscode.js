167b2ecddd4074c18d5018bd36da0cf3f93cb9b0
toshi-uy <s_borgia@hotmail.com>
2021-09-14 15:03:50 -0300

    testing status

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,6 +1,9 @@
 #!/usr/bin/node
 const url = process.argv[2];
 request(url)
-.on('response', function(response) {
-    console.log("Code:",response.statusCode)
+const options = {
+  uri: url
+}
+request(options, function (response) {
+  console.log('Code:', response.statusCode); 
 });
