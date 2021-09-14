0e7188e44f952399d8eb4b84823e5b4593ce4289
toshi-uy <s_borgia@hotmail.com>
2021-09-14 16:35:36 -0300

    added throw

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -2,6 +2,6 @@
 const argv1 = process.argv[2];
 const argv2 = process.argv[3];
 const fs = require('fs');
-fs.appendFile(argv2, argv1, function (err) {
+fs.appendFile(argv1, argv2, function (err) {
   if (err) throw err;
 });
