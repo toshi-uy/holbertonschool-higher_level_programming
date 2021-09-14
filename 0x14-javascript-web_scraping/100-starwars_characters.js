6f6cf2de9b41ae798b5d29c984cc954e800e100f
toshi-uy <s_borgia@hotmail.com>
2021-09-14 20:07:28 -0300

    what's inide

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const filmNbr = process.argv[2];
+const request = require('request');
+const url = 'https://swapi-api.hbtn.io/api/films/' + filmNbr;
+const options = { json: true };
+request(url, options, (error, res, body) => {
+  if (error) {
+    return console.log(error);
+  }
+  if (!error && res.statusCode === 200) {
+    console.log(body);
+  }
+});
