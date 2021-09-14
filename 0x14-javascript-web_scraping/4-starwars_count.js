2afbdf455b41e9e331524afe028317c9b2e0a325
toshi-uy <s_borgia@hotmail.com>
2021-09-14 16:07:19 -0300

    testing response

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -2,18 +2,13 @@
 const url = process.argv[2];
 const request = require('request');
 const options = { json: true };
-let character = 0;
 request(url, options, (error, res, body) => {
   if (error) {
     return console.log(error);
   }
   if (!error && res.statusCode === 200) {
     body.results.forEach(element => {
-      element.forEach(element => {
-        console.log(element)
-        }
-      });
+      console.log(element.characters)
     });
-    console.log(character);
   }
 });
