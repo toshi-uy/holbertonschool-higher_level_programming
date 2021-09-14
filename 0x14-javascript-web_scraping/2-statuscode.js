d7b0bb1378976b43a880e96e33ec83ace6b5c20b
toshi-uy <s_borgia@hotmail.com>
2021-09-14 15:14:53 -0300

    added code:

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -2,5 +2,5 @@
 const url = process.argv[2];
 var request = require('request');
 request.get(url, function (err, response) {
-  console.log("Code:" + response.statusCode); 
+  console.log("Code: " + response.statusCode); 
 });
