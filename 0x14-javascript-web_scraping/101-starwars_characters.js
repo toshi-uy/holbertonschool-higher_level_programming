1c1d8385ba59a6ddee9bb4af394a6404ad92fd36
toshi-uy <s_borgia@hotmail.com>
2021-09-14 21:55:15 -0300

    trying await and async

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,8 +1,4 @@
 #!/usr/bin/node
-const filmNbr = process.argv[2];
-const request = require('request');
-const url = 'https://swapi-api.hbtn.io/api/films/' + filmNbr;
-
 function resolveAfter2Seconds(x) {
   return new Promise(resolve => {
     setTimeout(() => {
@@ -11,13 +7,22 @@ function resolveAfter2Seconds(x) {
   });
 }
 
-async function f1() {
+function f2() {
+  const filmNbr = process.argv[2];
+  const request = require('request');
+  const url = 'https://swapi-api.hbtn.io/api/films/' + filmNbr;
   request(url, function (error, response, body) {
-  if (error) {
-    console.error(error);
+    if (error) {
+      console.error(error);
+    }
+    const path = JSON.parse(body).characters;
+    return path;
   }
-  const path = JSON.parse(body).characters;
+,
+
+async function f1(path) {
   const x = await resolveAfter2Seconds(10);
+  path = f2()
   path.forEach(element => {
     request(element, function (error, body) {
       if (error) {
