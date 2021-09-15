cf85420820373ad72e877f5496b438c89900c7a7
toshi-uy <s_borgia@hotmail.com>
2021-09-14 21:38:02 -0300

    testing appending to a list

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -3,22 +3,27 @@ const filmNbr = process.argv[2];
 const request = require('request');
 const url = 'https://swapi-api.hbtn.io/api/films/' + filmNbr;
 
-request(url, function (error, response, body) {
-  if (error) {
-    console.error(error);
-  }
-  const path = JSON.parse(body).characters;
-  console.log(path);
-  let wholeArray = Object.keys(path).map(key => path[key]);
-  console.log(wholeArray);
+async function f3() {
+  request(url, function (error, body) {
+    if (error) {
+      console.error(error);
+    }
+    const path = JSON.parse(body).characters;
+    console.log(path);
+    let wholeArray = Object.keys(path).map(key => path[key]);
+    console.log(wholeArray);
 
-  for(i = 0; i < wholeArray.length; i++) {
-    request(wholeArray[i], function (error, response, body) {
-      if (error) {
-        console.error(error);
-      }
-      const name = JSON.parse(body).name;
-      console.log(name);
-    });
-  };
-});
+    for(i = 0; i < wholeArray.length; i++) {
+      request(wholeArray[i], function (error, body) {
+        var y = await 20;
+        if (error) {
+          console.error(error);
+        }
+        const name = JSON.parse(body).name;
+        console.log(name);
+      });
+    };
+  })
+}
+
+f3();
