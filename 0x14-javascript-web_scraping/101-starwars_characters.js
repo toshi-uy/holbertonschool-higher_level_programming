d7e9df8554da5065876772254ac42325f69dfa90
toshi-uy <s_borgia@hotmail.com>
2021-09-14 21:06:29 -0300

    testing 101

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -3,22 +3,18 @@ const filmNbr = process.argv[2];
 const request = require('request');
 const url = 'https://swapi-api.hbtn.io/api/films/' + filmNbr;
 
-request(url, function (error, res, body) {
+request(url, function (error, body) {
   if (error) {
     console.error(error);
   }
-  if (!error && res.statusCode === 200){
-    const sorted = JSON.parse(body).characters;
-    sorted.forEach(element => {
-      request(element, function (error, res, body) {
-        if (error) {
-          console.error(error);
-        }
-        if (!error && res.statusCode === 200){
-        const name = JSON.parse(body).name;
-        console.log(name);
+  const sorted = JSON.parse(body).characters;
+  sorted.forEach(element => {
+    request(element, function (error, body) {
+      if (error) {
+        console.error(error);
       }
-      });
+      const name = JSON.parse(body).name;
+      console.log(name);
     });
-  }
+  });
 });
