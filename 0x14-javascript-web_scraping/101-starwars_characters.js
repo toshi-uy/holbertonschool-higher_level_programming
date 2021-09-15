869a1b6e2354a3422fcf4e08b5a7355d68224024
toshi-uy <s_borgia@hotmail.com>
2021-09-14 21:41:54 -0300

    trying await and asyng

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -3,27 +3,35 @@ const filmNbr = process.argv[2];
 const request = require('request');
 const url = 'https://swapi-api.hbtn.io/api/films/' + filmNbr;
 
-async function f3() {
-  request(url, function (error, body) {
-    if (error) {
-      console.error(error);
-    }
-    const path = JSON.parse(body).characters;
-    console.log(path);
-    let wholeArray = Object.keys(path).map(key => path[key]);
-    console.log(wholeArray);
-
-    for(i = 0; i < wholeArray.length; i++) {
-      request(wholeArray[i], function (error, body) {
-        if (error) {
-          console.error(error);
-        }
-        const name = JSON.parse(body).name;
-        name = await name;
-        console.log(name);
-      });
-    };
-  })
+function resolveAfter2Seconds(x) {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve(x);
+    }, 2000);
+  });
 }
 
-f3();
+async function f1() {
+  var x = await resolveAfter2Seconds(10);
+  request(url, function (error, response, body) {
+  if (error) {
+    console.error(error);
+  }
+  const path = JSON.parse(body).characters;
+  console.log(path);
+  let wholeArray = Object.keys(path).map(key => path[key]);
+  console.log(wholeArray);
+
+  for(i = 0; i < wholeArray.length; i++) {
+    request(wholeArray[i], function (error, response, body) {
+      if (error) {
+        console.error(error);
+      }
+      const name = JSON.parse(body).name;
+      console.log(name);
+    });
+  };
+  });
+}
+
+f1();
