94988254ead59f39f6e95c9230e7712eef462312
toshi-uy <s_borgia@hotmail.com>
2021-09-22 13:27:11 -0300

    added for test

--- 0x15-javascript-web_jquery/0-script.js
+++ 0x15-javascript-web_jquery/0-script.js
@@ -1,3 +1,4 @@
-#!/usr/bin/node
-const note = document.querySelector('header');
-note.style.color = '#FF0000';
+function display() {
+    const note = document.querySelector('header');
+    note.style.color = '#FF0000';
+}
