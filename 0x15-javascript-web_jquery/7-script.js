f5ae08db173e9cef9ca9eafa475a6a04e592d85b
toshi-uy <s_borgia@hotmail.com>
2021-09-23 14:44:57 -0300

    added 7 to test

--- 0x15-javascript-web_jquery/7-script.js
+++ 0x15-javascript-web_jquery/7-script.js
@@ -1,1 +1,3 @@
-$('DIV#character').load('https://swapi-api.hbtn.io/api/people/5/?format=json', 'name');
+$.get('https://swapi-api.hbtn.io/api/people/5/?format=json', function (data) {
+  $('DIV#character').text(data.name)
+});
