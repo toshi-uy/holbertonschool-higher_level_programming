193f8244fa50adbcd2caef007ed1d3f08933b385
toshi-uy <s_borgia@hotmail.com>
2021-09-26 17:24:32 -0300

    added 103 and fixed 102

--- 0x15-javascript-web_jquery/102-script.js
+++ 0x15-javascript-web_jquery/102-script.js
@@ -1,6 +1,6 @@
 $(document).ready(function language () {
   $('INPUT#btn_translate').click(function () {
-    const language = $('INPUT#language_code').val()
+    const language = $('INPUT#language_code').val();
     $.get('https://fourtonfish.com/hellosalut/?lang=' + language, function (response) {
       $('DIV#hello').text(response.hello);
     });
---
+++ 0x15-javascript-web_jquery/103-script.js
@@ -0,0 +1,16 @@
+$(document).ready(function language_enter () {
+  $('INPUT#btn_translate').click(function () {
+    const language = $('INPUT#language_code').val();
+    $.get('https://fourtonfish.com/hellosalut/?lang=' + language, function (response) {
+      $('DIV#hello').text(response.hello);
+    });
+  });
+  $(document).on('keypress',function(e) {
+    if(e.which == 13) {
+      const language = $('INPUT#language_code').val();
+      $.get('https://fourtonfish.com/hellosalut/?lang=' + language, function (response) {
+        $('DIV#hello').text(response.hello);
+      });
+    }
+  });
+});
