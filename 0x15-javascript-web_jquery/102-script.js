e80a921b1f41f7070f48a6556ff2466ac20724c6
toshi-uy <s_borgia@hotmail.com>
2021-09-26 17:08:49 -0300

    added file 102 working

---
+++ 0x15-javascript-web_jquery/102-script.js
@@ -0,0 +1,8 @@
+$(document).ready(function language () {
+  $('INPUT#btn_translate').click(function () {
+    const language = $('INPUT#language_code').val()
+    $.get('https://fourtonfish.com/hellosalut/?lang=' + language, function (response) {
+      $('DIV#hello').text(response.hello);
+    });
+  });
+});
