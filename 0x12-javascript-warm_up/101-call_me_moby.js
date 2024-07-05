2769dc28a3104a91bc8bd0308dd79a409496170f
toshi-uy <s_borgia@hotmail.com>
2024-07-05 01:24:42 +0000

    fixing semistandard

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-function callMeMoby(times, func) {
-  for (let i = 0; i < times ; i++) {
+function callMeMoby (times, func) {
+  for (let i = 0; i < times; i++) {
     func();
   }
 }
