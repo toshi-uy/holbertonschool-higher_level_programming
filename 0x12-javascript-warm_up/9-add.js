69514c13656599eedf26748af416133c11bf0040
toshi-uy <s_borgia@hotmail.com>
2024-07-05 02:07:42 +0000

    fixing semicolon

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const first = parseInt(process.argv[2]);
 const second = parseInt(process.argv[3]);
-function add(a, b) {
-    console.log(a + b);
+function add (a, b) {
+  console.log(a + b);
 }
 add(first, second);
